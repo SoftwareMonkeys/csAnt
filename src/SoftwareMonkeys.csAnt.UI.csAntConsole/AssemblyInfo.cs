f94c0a71ad3db35954283076be49786696df2f06
John Rebbeck <johnrebbeck@gmail.com>
2012-10-27 13:10:22 +1000

    Added (and moved) AssemblyInfo.cs files

---
+++ src/SoftwareMonkeys.csAnt.UI.csAntConsole/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("SoftwareMonkeys.csAnt.UI.csAntConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SoftwareMonkeys.csAnt.UI.csAntConsole")]
+[assembly: AssemblyCopyright("Copyright 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
