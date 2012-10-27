a910ee9ce9f1c6f555efb5c6de55c5ee82ff1b19
John Rebbeck <johnrebbeck@gmail.com>
2012-10-27 14:46:37 +1000

    Added ImportFileNodesLib script

---
+++ scripts/ImportCustomLibs.disabled.cs
@@ -0,0 +1,30 @@
+// To use this file....
+// 1) Rename this file to "ImportCustomLibs.cs" (remove the .disabled. section of the file name).
+// 2) Create a script called "Import[LibName]Lib.cs" for each library to import (such as "ImportMyProjectLib.cs")
+// 3) Use the ExecuteScript(...) function (look at the example below) to call each import library script
+
+//css_ref ../../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+
+using System;
+using System.IO;
+using Microsoft.CSharp;
+using System.Diagnostics;
+using SoftwareMonkeys.csAnt;
+using System.Collections.Generic;
+using System.Reflection;
+
+class ImportCustomLibs : BaseScript
+{
+	public static void Main(string[] args)
+	{
+		new ImportCustomLibs().Start(args);
+	}
+	
+	public void Start(string[] args)
+	{
+		// TODO: Add custom library imports, using the ExecuteScript function
+
+		// Example:
+		//	ExecuteScript("Import[LibName]Lib"); // Exclude the .cs extension
+	}
+}
