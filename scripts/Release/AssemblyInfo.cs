12f00267e26fd334c579f930957eb075d0b91fe3
John Rebbeck <johnrebbeck@gmail.com>
2012-11-04 09:59:33 +1000

    Added script files for Update and Release scripts

---
+++ scripts/Release/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("SoftwareMonkeys.csAnt.ReleaseScript")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
+
+
---
+++ scripts/Update/UpdateScript.cs
@@ -0,0 +1,94 @@
+//css_ref ../../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+using System;
+using System.IO;
+using Microsoft.CSharp;
+using System.Diagnostics;
+using SoftwareMonkeys.csAnt;
+using System.Collections.Generic;
+
+class CreateReleaseScript : BaseScript
+{
+	public static void Main(string[] args)
+	{
+		new CreateReleaseScript().Start();
+	}
+	
+	public void Start()
+	{
+		var listDir = ProjectDirectory
+			+ Path.DirectorySeparatorChar
+			+ "rls";
+
+		// Loop through the folder containing release list files
+		foreach (string listFile in Directory.GetFiles(listDir, "*-list.txt"))
+		{
+			Console.WriteLine("----------------------------------------------------------------------");
+			Console.WriteLine("Release list file: " + listFile.Replace(ProjectDirectory, ""));
+
+			var files = new List<string>(File.ReadLines(listFile)).ToArray();		
+
+			if (files.Length > 0)
+			{
+				Console.WriteLine(" ");
+				Console.WriteLine("Patterns:");
+
+				foreach (string file in files)
+				{
+					Console.WriteLine("  " + file);
+				}
+
+				Console.WriteLine(" ");
+
+				var variation = Path.GetFileNameWithoutExtension(listFile).Replace("-list", "");
+
+				var dateStamp = "["
+					+ DateTime.Now.Year
+					+  "-"
+					+ DateTime.Now.Month
+					+ "-"
+					+ DateTime.Now.Day
+					+ "--"
+					+ DateTime.Now.Hour
+					+ "-"
+					+ DateTime.Now.Minute
+					+ "-"
+					+ DateTime.Now.Second
+					+ "]";
+
+				var zipFileName = ProjectName
+					+ "-"
+					+ variation
+					+ "-"
+					+ dateStamp
+					+ ".zip";
+
+				var zipFilePath = ProjectDirectory
+					+ Path.DirectorySeparatorChar
+					+ "rls"
+					+ Path.DirectorySeparatorChar
+					+ variation
+					+ Path.DirectorySeparatorChar
+					+ zipFileName;
+
+				if (!Directory.Exists(Path.GetDirectoryName(zipFilePath)))
+					Directory.CreateDirectory(Path.GetDirectoryName(zipFilePath));
+
+				Console.WriteLine("Zip file path: " + zipFilePath);
+
+				Zip(
+					files,
+					zipFilePath
+				);
+
+				Console.WriteLine("  Release file: " + zipFilePath.Replace(ProjectDirectory, ""));
+				Console.WriteLine("Release zip file created successfully.");
+				Console.WriteLine("");
+				Console.WriteLine("----------------------------------------------------------------------");
+			} 
+			else
+				Console.WriteLine("No files or patterns specified in the release file list.");
+
+			Console.WriteLine("");
+		}
+	}
+}
