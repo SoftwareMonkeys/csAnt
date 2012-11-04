a9ae35f266901c625a8f719b221e984b1e4dd398
John Rebbeck <johnrebbeck@gmail.com>
2012-11-05 10:14:59 +1100

    Added GoogleCodeUpload function for uploading to GoogleCode.com
    Signed-off-by: John Rebbeck <johnrebbeck@gmail.com>

--- src/SoftwareMonkeys.csAnt/BaseScript.GoogleCodeUpload.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.GoogleCodeUpload.cs
@@ -1,12 +1,95 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace SoftwareMonkeys.csAnt
 {
-	public class BaseScript
+	public partial class BaseScript
 	{
-		public BaseScript ()
+		public void GoogleCodeUpload(
+			string projectName,
+			string filePath,
+			string targetFileName
+		)
 		{
+			var gcSecurityNode = ProjectNode.Nodes["Security"].Nodes["GoogleCode"];
+			
+			var username = gcSecurityNode.Properties["Username"];
+
+			var password = gcSecurityNode.Properties["Password"];
+
+			GoogleCodeUpload(
+				projectName,
+				filePath,
+				targetFileName,
+				username,
+				password
+			);
+
 		}
+
+		public void GoogleCodeUpload(
+			string projectName,
+			string filePath,
+			string targetFileName,
+			string username,
+			string password
+		)
+		{
+			// TODO: Check if this needs to be modified to run on Windows .NET
+			var command = "mono";
+
+			var csUploadExePath = ProjectDirectory
+				+ Path.DirectorySeparatorChar
+				+ "lib"
+				+ Path.DirectorySeparatorChar
+				+ "GCUpload"
+				+ Path.DirectorySeparatorChar
+				+ "GCUpload.exe";
+
+			List<string> arguments = new List<string>();
+			
+			arguments.Add(
+				"'" + csUploadExePath + "'"
+			);
+
+			arguments.Add(
+				"-s:"
+				+ "'" + Path.GetFileName (filePath) + "'"
+			);
+
+			arguments.Add(
+				"-n:"
+				+ projectName
+			);
+
+			arguments.Add(
+				"-u:"
+				+ username
+			);
+
+			arguments.Add(
+				"-p:"
+				+ password
+			);
+			
+			arguments.Add(
+				"-f:"
+				+ "'" + filePath + "'" 
+			);
+			
+			arguments.Add(
+				"-t:"
+				+ "'" + targetFileName + "'" 
+			);
+
+			StartProcess (
+				command,
+			    arguments.ToArray()
+			);
+
+		}
+			
 	}
 }
 
