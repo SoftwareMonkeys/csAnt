b54cefc2a167ef25e79fab51bb63d20f29888513
John Rebbeck <johnrebbeck@gmail.com>
2012-11-04 10:37:06 +1000

    Added FileEquals function for comparing two files to check if their content is equal

--- src/SoftwareMonkeys.csAnt/BaseScript.FileEquals.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.FileEquals.cs
@@ -1,11 +1,34 @@
 using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace SoftwareMonkeys.csAnt
 {
-	public class BaseScript
+	public partial class BaseScript
 	{
-		public BaseScript ()
+		public bool FileEquals(string filePath, string filePath2)
 		{
+			var fileHash1 = GetMD5HashFromFile(filePath);
+			var fileHash2 = GetMD5HashFromFile(filePath2);
+
+			return fileHash1.Equals(fileHash2);
+		}
+
+		// Function code courtesy of: http://sharpertutorials.com/calculate-md5-checksum-file/
+		protected string GetMD5HashFromFile(string fileName)
+		{
+			FileStream file = new FileStream(fileName, FileMode.Open);
+			MD5 md5 = new MD5CryptoServiceProvider();
+			byte[] retVal = md5.ComputeHash(file);
+			file.Close();
+
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < retVal.Length; i++)
+			{
+				sb.Append(retVal[i].ToString("x2"));
+			}
+			return sb.ToString();
 		}
 	}
 }
