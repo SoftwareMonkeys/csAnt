44289ee3906d9803f2388b2c26286740360c48ad
John Rebbeck <johnrebbeck@gmail.com>
2012-11-04 10:36:07 +1000

    Added ScrapListFromXPath function for reading an array of text off a web page

--- src/SoftwareMonkeys.csAnt/BaseScript.ScrapeListFromXPath.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.ScrapeListFromXPath.cs
@@ -1,11 +1,59 @@
 using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
 
 namespace SoftwareMonkeys.csAnt
 {
-	public class BaseScript
+	public partial class BaseScript
 	{
-		public BaseScript ()
+		/// <summary>
+		/// Extracts an array of text from the specified URL, from the InnerContent property of the nodes matching the provided XPath query.
+		/// </summary>
+		/// <returns>
+		/// 
+		/// </returns>
+		/// <param name='url'>
+		/// 
+		/// </param>
+		/// <param name='xpath'>
+		/// 
+		/// </param>
+		public string[] ScrapeXPathArray(
+			string url,
+			string xpath
+		)
 		{
+			var web = new HtmlWeb();
+
+			var doc = web.Load(url);
+
+			var nodes = doc.DocumentNode.SelectNodes(xpath);
+
+			List<string> values = new List<string>();
+
+			if (IsVerbose)
+			{
+				Console.WriteLine("XPath: " + xpath);
+			}
+
+			if (nodes != null)
+			{
+				if (IsVerbose)
+					Console.WriteLine("Total nodes: " + nodes.Count);
+
+				foreach (var node in nodes)
+				{
+					if (!String.IsNullOrEmpty(node.InnerText.Trim ()))
+						values.Add (node.InnerText.Trim ());
+				}
+			}
+			else
+			{
+				if (IsVerbose)
+					Console.WriteLine("No nodes found.");
+			}
+
+			return values.ToArray();
 		}
 	}
 }
