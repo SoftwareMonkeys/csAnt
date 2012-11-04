dcee2e27cc9d86e51886a03339734e18e5be364f
John Rebbeck <johnrebbeck@gmail.com>
2012-11-05 10:03:59 +1100

    Added various BaseScript functions and properties

---
+++ src/SoftwareMonkeys.csAnt/BaseScript.StartNewProcess.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Diagnostics;
+
+namespace SoftwareMonkeys.csAnt
+{
+	public partial class BaseScript
+	{
+		/// <summary>
+		/// Starts/executes a process in a new thread.
+		/// </summary>
+		/// <returns>
+		/// The newly started process.
+		/// </returns>
+		/// <param name='command'></param>
+		/// <param name='arguments'></param>
+		public Process StartNewProcess(string command, params string[] arguments)
+		{
+			return StartNewProcess(command, String.Join (" ", arguments));
+		}
+
+		/// <summary>
+		/// Starts/executes a process in a new thread.
+		/// </summary>
+		/// <returns>
+		/// The newly started process.
+		/// </returns>
+		/// <param name='command'></param>
+		/// <param name='arguments'></param>
+		public Process StartNewProcess(string command, string arguments)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("Starting new process: " + command);
+			Console.WriteLine("Arguments:");
+			Console.WriteLine(arguments);
+			Console.WriteLine("");
+
+			// Create the process start info
+			ProcessStartInfo info = new ProcessStartInfo(command, arguments);
+
+			// Configure the process
+			info.UseShellExecute = true;
+
+			info.RedirectStandardOutput = false;
+			info.RedirectStandardError = false;
+			info.RedirectStandardInput = false;
+
+			info.CreateNoWindow = false;
+
+			info.ErrorDialog = false;
+
+	        info.WindowStyle = ProcessWindowStyle.Normal;
+
+			// Start the process
+			Process process = Process.Start(info);
+
+			return process;
+
+		}
+	}
+}
+
