3c1a8befec48c1fe76a980f759bf2df106272ed5
John Rebbeck <johnrebbeck@gmail.com>
2012-11-05 10:07:02 +1100

    Added required files

---
+++ src/SoftwareMonkeys.csAnt/BaseScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SoftwareMonkeys.csAnt
+{
+	/// <summary>
+	/// Used as the base of C# scripts.
+	/// </summary>
+	public abstract partial class BaseScript
+	{
+		public BaseScript()
+		{
+		}
+	}
+}
