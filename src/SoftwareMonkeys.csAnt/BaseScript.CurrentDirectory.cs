db070f6a146a3b21069f949ce1d5fd753e076d0b
John Rebbeck <johnrebbeck@gmail.com>
2012-11-05 08:32:58 +1000

    Added BuildSolution function

---
+++ src/SoftwareMonkeys.csAnt/BaseScript.CurrentDirectory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace SoftwareMonkeys.csAnt
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public partial class BaseScript
+	{
+		private string currentDirectory = String.Empty;
+		public string CurrentDirectory
+		{
+			get
+			{
+				if (String.IsNullOrEmpty(currentDirectory))
+				{
+					currentDirectory = Environment.CurrentDirectory;
+				}
+				return currentDirectory;
+			}
+			set
+			{
+				currentDirectory = value;
+			}
+		}
+	}
+}
