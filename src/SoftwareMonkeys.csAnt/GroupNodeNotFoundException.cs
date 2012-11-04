5a433b02da2896b10ae1a17fab62e816be78db21
John Rebbeck <johnrebbeck@gmail.com>
2012-11-05 10:07:31 +1100

    Added GroupNodeNotFoundException

---
+++ src/SoftwareMonkeys.csAnt/GroupNodeNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SoftwareMonkeys.csAnt
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class GroupNodeNotFoundException : Exception
+	{
+		public GroupNodeNotFoundException() : base("No group node found. Ensure a [GroupName].node file exists in the group/company directory (the directory one step up from the project).")
+		{
+		}
+	}
+}
