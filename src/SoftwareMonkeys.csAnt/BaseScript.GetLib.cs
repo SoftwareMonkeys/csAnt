a186114f278bd6ab748b211ad6b5913488006ac1
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-05 13:08:42 +1000

    Added new library related functions

--- src/SoftwareMonkeys.csAnt/BaseScript.GetLib.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.GetLib.cs
@@ -1,11 +1,55 @@
 using System;
+using System.IO;
 
 namespace SoftwareMonkeys.csAnt
 {
-	public class BaseScript
+	public partial class BaseScript
 	{
-		public BaseScript ()
+		public void GetLib(string name)
 		{
+			GetLib (name, false);
+		}
+
+		public void GetLib(string name, bool force)
+		{
+			Console.WriteLine ("");
+			Console.WriteLine ("Retrieving library: " + name);
+			Console.WriteLine ("");
+
+			if (ProjectNode.Nodes["Libraries"].Nodes.ContainsKey(name))
+			{
+				var projectNode = ProjectNode.Nodes["Libraries"].Nodes[name];
+
+				string url = projectNode.Properties["Url"];
+				string subPath = String.Empty;
+
+				if (projectNode.Properties.ContainsKey("SubPath"))
+					subPath = projectNode.Properties["SubPath"];
+			
+				var libsPath = ProjectDirectory
+					+ Path.DirectorySeparatorChar
+					+ "lib";
+
+				var libPath = libsPath
+					+ Path.DirectorySeparatorChar
+					+ name;
+
+				// TODO: Add a date/time stamp in the file name
+				var zipFilePath = libPath
+					+ Path.DirectorySeparatorChar
+					+ name
+					+ ".zip";
+
+				DownloadAndUnzip(
+					url,
+					zipFilePath,
+					libPath,
+					subPath,
+					force
+				);
+			}
+			else
+				Console.WriteLine("Error: Library not found: '" + name + "'. Add it using 'AddLib [name] [url]'.");
 		}
 	}
 }
