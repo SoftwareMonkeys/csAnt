0cfab5a8e7f05ef4de60703714e54df75f0eb5b4
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-05 13:11:08 +1000

    Added AddLib script

---
+++ scripts/AddLib.cs
@@ -0,0 +1,32 @@
+//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+using System;
+using System.IO;
+using Microsoft.CSharp;
+using System.Diagnostics;
+using SoftwareMonkeys.csAnt;
+
+class AddLibScript : BaseScript
+{
+	public static void Main(string[] args)
+	{
+		new AddLibScript().Start(args);
+	}
+	
+	public void Start(string[] args)
+	{
+		if (args.Length < 2)
+			Console.WriteLine("Two arguments expected; name and URL (to zip file).");
+		else
+		{
+			string name = args[0];
+
+			string url = args[1];
+
+			string subPath = String.Empty;
+			if (args.Length == 3)
+				subPath = args[2];
+
+			AddLib(name, url, subPath);
+		}
+	}
+}
