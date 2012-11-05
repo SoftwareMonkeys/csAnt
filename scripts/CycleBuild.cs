721c8b4979616b13d39adc567e9ac1f6ac2ba8e8
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-05 15:07:54 +1100

    Added cycle scripts

---
+++ scripts/CycleBuild.cs
@@ -0,0 +1,38 @@
+//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+using System;
+using System.IO;
+using Microsoft.CSharp;
+using System.Diagnostics;
+using SoftwareMonkeys.csAnt;
+
+class CycleBuildScript : BaseScript
+{
+	public static void Main(string[] args)
+	{
+		new CycleBuildScript().Start();
+	}
+	
+	public void Start()
+	{
+		Console.WriteLine("");
+		Console.WriteLine("Starting a full build cycle.");
+		Console.WriteLine("");
+
+		Console.WriteLine("Building...");
+		Console.WriteLine("");
+
+		// Build the solution
+		ExecuteScript(
+			"BuildSolution",
+			new string[]{
+				ProjectName + ".MonoDevelop"
+			}
+		);
+
+		// Copy the binaries to the libraries folder
+		ExecuteScript(
+			"CopyBinToLib"
+		);
+		
+	}
+}
