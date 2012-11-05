f8e86d817230740e4c834916317f95365ea8e4c2
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-05 15:07:54 +1100

    Added cycle scripts

---
+++ scripts/CycleRelease.cs
@@ -0,0 +1,41 @@
+//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+using System;
+using System.IO;
+using Microsoft.CSharp;
+using System.Diagnostics;
+using SoftwareMonkeys.csAnt;
+
+class CycleReleaseScript : BaseScript
+{
+	public static void Main(string[] args)
+	{
+		new CycleReleaseScript().Start();
+	}
+	
+	public void Start()
+	{
+		Console.WriteLine("");
+		Console.WriteLine("Starting a full release cycle.");
+		Console.WriteLine("");
+
+		ExecuteScript("CycleBuild");
+
+		Console.WriteLine("Creating release zip files...");
+		Console.WriteLine("");
+
+		// Create the release
+		ExecuteScript(
+			"Release",
+			new string[]{
+				"-mode:Release"
+			}
+		);
+
+		Console.WriteLine("Uploading the release zip file to GoogleCode...");
+		Console.WriteLine("");
+
+		// Upload to GoogleCode
+		ExecuteScript("GoogleCodeRelease");
+		
+	}
+}
