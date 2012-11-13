22bf5ea2fd095ba41d54d255523e82646117555e
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-13 11:41:27 +1000

    Fixed cycle scripts

--- scripts/CycleRelease.cs
+++ scripts/CycleRelease.cs
@@ -1,11 +1,14 @@
 //css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.dll;
+
 using System;
 using System.IO;
 using Microsoft.CSharp;
 using System.Diagnostics;
 using SoftwareMonkeys.csAnt;
+using SoftwareMonkeys.csAnt.Projects;
 
-class CycleReleaseScript : BaseScript
+class CycleReleaseScript : BaseProjectScript
 {
 	public static void Main(string[] args)
 	{
