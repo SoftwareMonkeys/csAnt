77cc8b2c1965d1772df26664b0baad499bafe10b
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-13 11:10:38 +1000

    Execute function now returns the Process object

--- src/SoftwareMonkeys.csAnt/BaseScript.Execute.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.Execute.cs
@@ -8,15 +8,15 @@ namespace SoftwareMonkeys.csAnt
 	/// </summary>
 	public partial class BaseScript
 	{
-		public void Execute(string command, params string[] arguments)
+		public Process Execute(string command, params string[] arguments)
 		{
-			Execute(command, String.Join(" ", arguments));
+			return Execute(command, String.Join(" ", arguments));
 		}
 		
-		public void Execute(string command, string arguments)
+		public Process Execute(string command, string arguments)
 		{
 			// Use the StartProcess function (this Execute function is just an alias)
-			StartProcess(command, arguments);
+			return StartProcess(command, arguments);
 		}
 	}
 }
