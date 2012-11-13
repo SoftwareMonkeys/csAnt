3cf710474bde359b8aa9a328b7990df6742e90d4
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-13 11:18:03 +1000

    Updated Release script

--- scripts/Release/ReleaseScript.cs
+++ scripts/Release/ReleaseScript.cs
@@ -1,16 +1,18 @@
 //css_ref ../../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+//css_ref ../../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.dll;
 using System;
 using System.IO;
 using Microsoft.CSharp;
 using System.Diagnostics;
 using SoftwareMonkeys.csAnt;
+using SoftwareMonkeys.csAnt.Projects;
 using System.Collections.Generic;
 
-class CreateReleaseScript : BaseScript
+class ReleaseScript : BaseProjectScript
 {
 	public static void Main(string[] args)
 	{
-		new CreateReleaseScript().Start();
+		new ReleaseScript().Start();
 	}
 	
 	public void Start()
