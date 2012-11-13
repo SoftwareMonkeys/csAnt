53117392548c4ca6ce8dba1add170a386139d6d3
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-13 12:14:14 +1000

    Fixed update script

--- scripts/Update/UpdateScript.cs
+++ scripts/Update/UpdateScript.cs
@@ -1,12 +1,15 @@
 //css_ref ../../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+//css_ref ../../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.dll;
+
 using System;
 using System.IO;
 using Microsoft.CSharp;
 using System.Diagnostics;
 using SoftwareMonkeys.csAnt;
+using SoftwareMonkeys.csAnt.Projects;
 using System.Collections.Generic;
 
-class UpdateScript : BaseScript
+class UpdateScript : BaseProjectScript
 {
 	public static void Main(string[] args)
 	{
