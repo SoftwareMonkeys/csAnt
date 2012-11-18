ed50d8f9da32b7687d54a232e5b0d37a8a577962
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-18 13:57:40 +1000

    Added GetZipInternalPathTestFixture

--- src/SoftwareMonkeys.csAnt.Tests/GetZipInternalPathTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Tests/GetZipInternalPathTestFixture.cs
@@ -1,11 +1,38 @@
 using System;
+using NUnit.Framework;
+using System.IO;
 
 namespace SoftwareMonkeys.csAnt.Tests
 {
+	[TestFixture]
 	public class GetZipInternalPathTestFixture
 	{
-		public GetZipInternalPathTestFixture ()
+		[Test]
+		public void Test_GetZipInternalPath()
 		{
+			string fileName = "/SourceDir/File.txt";
+
+			string destination = "/DestDir/";
+
+			string pattern = fileName + "|" + destination;
+
+			string zipFileName = "MyZipFile";
+
+			var script = new TestScript();
+
+			string internalPath = script.GetZipInternalPath(
+				zipFileName,
+				fileName,
+				pattern
+			);
+
+			var expected = zipFileName
+				+ "/"
+				+ destination.Trim('/')
+				+ "/"
+				+ Path.GetFileName(fileName);
+
+			Assert.AreEqual(expected, internalPath, "Wrong path returned.");
 		}
 	}
 }
