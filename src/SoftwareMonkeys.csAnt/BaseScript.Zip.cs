2b4f2cc3f88ccb31a409b13b9d4dbe231501eac4
CompulsiveCoder <compulsivecoder@gmail.com>
2012-11-18 13:40:21 +1000

    Zip function now allows files to be put in a different location within the zip file

--- src/SoftwareMonkeys.csAnt/BaseScript.Zip.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.Zip.cs
@@ -6,6 +6,20 @@ namespace SoftwareMonkeys.csAnt
 {
 	public partial class BaseScript
 	{
+		/// <summary>
+		/// Zips the files matching the specified file patterns.
+		/// </summary>
+		/// <param name='filePatterns'>
+		/// An array of patterns used to match the files being zipped.
+		/// Example patterns:
+		/// /SourceFolder/MyFile.txt - single file
+		/// /SourceFolder/* - multiple files with a wildcard
+		/// /SourceFolder/*.txt - multiple files of a particular type with a wildcard
+		/// /SourceFolder/MyFile.txt|/NewLocation/ - specifies a new location for the file within the zip (wildcards can also be used)
+		/// </param>
+		/// <param name='zipFilePath'>
+		/// 
+		/// </param>
 		public void Zip(string[] filePatterns, string zipFilePath)
 		{
 			if (!Directory.Exists(Path.GetDirectoryName(zipFilePath)))
@@ -25,11 +39,18 @@ namespace SoftwareMonkeys.csAnt
 
             byte[] buffer = new byte[4096];
 
-            foreach (string pattern in filePatterns)
+            foreach (string patternLine in filePatterns)
             {
-				if (!String.IsNullOrEmpty(pattern))
+				if (!String.IsNullOrEmpty(patternLine))
 				{
-					Console.WriteLine ("  Specified pattern/file: " + pattern);
+					Console.WriteLine ("  Specified pattern/file: " + patternLine);
+
+					string pattern = patternLine;
+
+					if (patternLine.IndexOf("|") > -1)
+					{
+						pattern = patternLine.Split('|')[0];
+					}
 
 					string shortPattern = pattern.Replace(CurrentDirectory, "");
 
@@ -43,9 +64,11 @@ namespace SoftwareMonkeys.csAnt
 					{
 						Console.WriteLine ("    " + foundFile.Replace (CurrentDirectory, ""));
 
-						var internalPath = zipFileName
-							+ "/"
-							+ foundFile.Replace(CurrentDirectory, "");
+						var internalPath = GetZipInternalPath(
+							zipFileName,
+							foundFile,
+							patternLine
+						);
 
 		                ZipEntry entry = new ZipEntry(
 							internalPath
@@ -74,6 +97,27 @@ namespace SoftwareMonkeys.csAnt
             s.Finish();
             s.Close();
 
+		}
+
+		public string GetZipInternalPath(string zipName, string fileName, string originalPattern)
+		{
+			var subPath = String.Empty;
+
+			if (originalPattern.IndexOf("|") > -1)
+			{
+				var patternParts = originalPattern.Split('|');
+
+				subPath = patternParts[1]
+					+ Path.GetFileName(fileName);
+			}
+			else
+			{
+				subPath = fileName.Replace(CurrentDirectory, "");
+			}
+
+			return zipName
+				+ "/"
+				+ subPath.Trim('/');
 		}
 	}
 }
