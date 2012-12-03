13153d199b72f39154a1d0018ead28a40cb1f76f
CompulsiveCoder <compulsivecoder@gmail.com>
2012-12-04 07:12:07 +1000

    Updated methods to use new commands

--- src/SoftwareMonkeys.csAnt/BaseScript.Download.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.Download.cs
@@ -22,46 +22,6 @@ namespace SoftwareMonkeys.csAnt
 			ExecuteCommand(cmd);
 
 			return (string)cmd.ReturnValue;
-
-			/*Console.WriteLine ("Downloading...");
-			Console.WriteLine ("  From URL: " + url);
-
-			var fileName = Path.GetFileName(url);
-
-			var toFile = localDestination;
-
-			if (localDestination.IndexOf("/") == localDestination.Length-1)
-				toFile = localDestination + fileName;
-			
-			Console.WriteLine ("  To file: " + toFile);
-
-			WebClient webClient = new WebClient();
-
-			webClient.Headers.Add("USER-AGENT", "csAnt");
-
-			webClient.Credentials = CredentialCache.DefaultCredentials;
-
-			Console.WriteLine ("  Please wait...(this may take some time)...");
-
-			webClient.DownloadFile(
-				url,
-				toFile
-			);
-
-			var size = Convert.ToInt32(webClient.ResponseHeaders["Content-Length"]);
-
-			var sizeString = size + "b";
-
-			if (size > 1000*1000)
-				sizeString = size / 1000 / 1000 + "mb";
-			else if (size > 1000)
-				sizeString = size / 1000 + "kb";
-
-			Console.WriteLine ("  Size: " + sizeString);
-
-			Console.WriteLine ("Download complete.");
-
-			return toFile;*/
 		}
 	}
 }
--- src/SoftwareMonkeys.csAnt/BaseScript.DownloadAndUnzip.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.DownloadAndUnzip.cs
@@ -28,52 +28,6 @@ namespace SoftwareMonkeys.csAnt
 			cmd.Force = force;
 
 			ExecuteCommand(cmd);
-
-			/*// Create the _tmp directory if it doesn't exist
-			if (!Directory.Exists(Path.GetDirectoryName(zipFileLocalPath)))
-				Directory.CreateDirectory(Path.GetDirectoryName(zipFileLocalPath));
-
-			Console.WriteLine ("Zip file path: " + zipFileLocalPath);
-
-			// If the zip file doesn't already exist
-			if (!File.Exists(zipFileLocalPath)
-			    // Or the force flag is true
-				|| force)
-			{
-				// Download the zip file to the temporary location
-				Download (zipFileUrl, zipFileLocalPath);
-			}
-			else
-			{				
-				Console.WriteLine("Zip file already found locally. Skipping download.");
-			}
-
-			// Create a temporary folder name
-			var tmpFolder = CurrentDirectory
-				+ Path.DirectorySeparatorChar
-				+ "_tmp"
-				+ Path.DirectorySeparatorChar
-				+ "tmp-" + Guid.NewGuid().ToString();
-
-			// Unzip the zip file
-			Unzip (zipFileLocalPath, tmpFolder);
-
-			var fullSubPath = Path.GetFullPath(
-				tmpFolder
-				+ Path.DirectorySeparatorChar
-				+ subPath
-			);
-			
-			Console.WriteLine ("");
-			Console.WriteLine ("Moving files to: ");
-			Console.WriteLine ("  " + localDirectory);
-			Console.WriteLine ();
-
-			MoveDirectory(fullSubPath, localDirectory);
-			
-			// Delete the temporary folder
-			if (Directory.Exists(tmpFolder))
-				Directory.Delete(tmpFolder);*/
 		}
 	}
 }
