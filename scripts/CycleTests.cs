0ef3b193a9be5416b3199c495f15b304872b043b
CompulsiveCoder <compulsivecoder@gmail.com>
2013-08-08 10:08:08 +1000

    Added CycleTests script

---
+++ scripts/CycleTests.cs
@@ -0,0 +1,33 @@
+//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.dll;
+
+using System;
+using System.IO;
+using Microsoft.CSharp;
+using System.Diagnostics;
+using SoftwareMonkeys.csAnt;
+using SoftwareMonkeys.csAnt.Projects;
+
+class CycleTestsScript : BaseProjectScript
+{
+	public static void Main(string[] args)
+	{
+		new CycleTestsScript().Start(args);
+	}
+	
+	public override bool Start(string[] args)
+	{
+		Console.WriteLine("");
+		Console.WriteLine("Starting a full release cycle.");
+		Console.WriteLine("");
+
+		ExecuteScript("CycleBuild");
+
+		Console.WriteLine("Running the tests...");
+		Console.WriteLine("");
+
+		ExecuteScript("RunTests");
+
+		return !IsError;
+	}
+}
