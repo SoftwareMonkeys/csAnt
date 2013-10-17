ce40b02ac7c65160b427ebadd5368361029e7443
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-18 08:20:02 +1000

    Updated BaseProjectsTestFixture

--- src/SoftwareMonkeys.csAnt.Projects.Tests/BaseProjectsTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Projects.Tests/BaseProjectsTestFixture.cs
@@ -1,11 +1,31 @@
 using System;
+using SoftwareMonkeys.csAnt.Tests;
+using System.IO;
 
 namespace SoftwareMonkeys.csAnt.Projects.Tests
 {
-	public class BaseProjectsTestFixture
+	public class BaseProjectsTestFixture : BaseTestFixture
 	{
+		public string ProjectDirectory { get; set; }
+
 		public BaseProjectsTestFixture ()
 		{
+			ProjectDirectory = Path.GetFullPath("../../../../");
+		}
+
+		public TestProjectScript GetTestScript()
+		{
+			var testScript = new TestProjectScript();
+			
+			testScript.IsVerbose = true;
+
+			var actualProjectDir = ProjectDirectory;
+
+			testScript.ProjectDirectory = testScript.GetTmpDir();
+
+			testScript.CopyTestFiles(actualProjectDir, testScript.ProjectDirectory);
+
+			return testScript;
 		}
 	}
 }
