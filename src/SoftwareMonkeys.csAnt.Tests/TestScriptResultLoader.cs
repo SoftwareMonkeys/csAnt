68aa3c6749aa493fbff3c8f5d8d601858a49e235
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-22 17:36:32 +1000

    Added/updated test related files

--- src/SoftwareMonkeys.csAnt.Tests/TestScriptResultLoader.cs
+++ src/SoftwareMonkeys.csAnt.Tests/TestScriptResultLoader.cs
@@ -1,11 +1,34 @@
 using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace SoftwareMonkeys.csAnt.Tests
 {
 	public class TestScriptResultLoader
 	{
-		public TestScriptResultLoader ()
+		public string ReportsDirectory { get;set; }
+
+		public TestScriptResultLoader (
+			string reportsDir
+		)
 		{
+			ReportsDirectory = reportsDir;
+		}
+
+		public TestScriptResult Load(
+			string xmlResultFile
+		)
+		{
+			TestScriptResult result = null;
+
+			using (var streamReader = File.OpenRead(xmlResultFile))
+			{
+				var xmlSerializer = new XmlSerializer(typeof(TestScriptResult));
+
+				result = (TestScriptResult)xmlSerializer.Deserialize(streamReader);
+			}
+
+			return result;
 		}
 	}
 }
