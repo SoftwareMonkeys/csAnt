d17c9680e2c5726df95eaaa879ed85731830d63b
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-22 19:51:45 +1000

    Updated SubConsoleWriter

--- src/SoftwareMonkeys.csAnt/SubConsoleWriter.cs
+++ src/SoftwareMonkeys.csAnt/SubConsoleWriter.cs
@@ -2,10 +2,47 @@ using System;
 
 namespace SoftwareMonkeys.csAnt
 {
-	public class SubConsoleWriter
+	public class SubConsoleWriter : ConsoleWriter
 	{
-		public SubConsoleWriter ()
+		public ConsoleWriter ParentWriter { get;set; }
+
+		public SubConsoleWriter (ConsoleWriter parentWriter, string scriptName)
 		{
+			ParentWriter = parentWriter;
+
+			ScriptName = scriptName;
+		}
+
+		public override void WriteLine (string text)
+		{
+			if (text == null)
+				text = String.Empty;
+
+			// TODO: Increase the indent depending on the indent of the script
+
+			if (ParentWriter != null)
+				ParentWriter.WriteLine (text);
+			else {
+				System.Console.WriteLine (text);
+				AppendOutputFile(text);
+			}
+
+			AppendOutput(text + "\n");
+		}
+
+		public override void Write (string text)
+		{
+			if (text == null)
+				text = String.Empty;
+
+			if (ParentWriter != null)
+				ParentWriter.Write (text);
+			else {
+				System.Console.Write (text);
+				AppendOutputFile(text);
+			}
+
+			AppendOutput(text);
 		}
 	}
 }
