00671b2513d2b73524c1011bea7911dadb2a4a99
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-22 17:30:42 +1000

    Added GitSyncTestFixture

--- src/SoftwareMonkeys.csAnt.Projects.Tests/GitSyncTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Projects.Tests/GitSyncTestFixture.cs
@@ -1,11 +1,37 @@
 using System;
+using System.IO;
+using NUnit.Framework;
 
 namespace SoftwareMonkeys.csAnt.Projects.Tests
 {
-	public class GitSyncTestFixture
+	[TestFixture]
+	public class GitSyncTestFixture : BaseProjectsTestFixture
 	{
-		public GitSyncTestFixture ()
+		[Test]
+		public void Test_GitSync()
 		{
+			var testScript = GetTestScript();
+
+			Console.WriteLine ("Project dir: " + testScript.ProjectDirectory);
+
+			// Sync the files
+			testScript.GitSync(
+				"csAnt",
+				ProjectDirectory // Use the actual project directory from the test fixture, not the temporary directory from the test script
+			);
+
+			var tmpFile = testScript.ProjectDirectory
+				+ Path.DirectorySeparatorChar
+				+ "TestFile.txt";
+
+			File.WriteAllText(tmpFile, "Test content");
+
+			// Sync the files again
+			testScript.GitSync(
+				"csAnt",
+				ProjectDirectory // Use the actual project directory from the test fixture, not the temporary directory from the test script
+			);
+
 		}
 	}
 }
