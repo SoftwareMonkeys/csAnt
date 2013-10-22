e0447f0c4acc00b3e5c51eb018f4ec12449d14ab
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-22 17:28:45 +1000

    Added BaseProjectTestScript and TestProjectScript

--- src/SoftwareMonkeys.csAnt.Projects.Tests/TestProjectScript.cs
+++ src/SoftwareMonkeys.csAnt.Projects.Tests/TestProjectScript.cs
@@ -2,10 +2,15 @@ using System;
 
 namespace SoftwareMonkeys.csAnt.Projects.Tests
 {
-	public class TestProjectScript
+	public class TestProjectScript : BaseProjectTestScript
 	{
 		public TestProjectScript ()
 		{
+		}
+
+		public override bool Start (string[] args)
+		{
+			throw new System.NotImplementedException ();
 		}
 	}
 }
