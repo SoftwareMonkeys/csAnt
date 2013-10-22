e6a285e70330b6c364a88efae7de8e3b36111e6a
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-22 17:13:45 +1000

    Added RunUnitTestsScript

---
+++ scripts/RunUnitTests/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("SoftwareMonkeys.csAnt.RunTestsScript")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
