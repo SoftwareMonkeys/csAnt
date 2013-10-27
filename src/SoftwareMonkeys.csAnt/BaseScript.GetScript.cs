233629a944df42a8a3fd8771c87c5eca365d2fc4
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-27 21:36:32 +1000

    Moved script creation to ActivateScript function

--- src/SoftwareMonkeys.csAnt/BaseScript.GetScript.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.GetScript.cs
@@ -11,33 +11,7 @@ namespace SoftwareMonkeys.csAnt
 		{
 			var scriptPath = GetScriptPath(scriptName);
 
-			return GetScriptFromPath(scriptPath);
-		}
-		
-		public IScript GetScriptFromPath(string scriptPath)
-		{
-			CSScript.CacheEnabled = false;
-
-			// Load the script assembly
-			Assembly a = CSScript.Load (scriptPath);
-
-			var scriptName = Path.GetFileNameWithoutExtension(scriptPath);
-
-			// Get the script type
-			var type = a.GetTypes () [0];
-
-			var b = (IScript)a.CreateInstance (type.Name);
-
-			// TODO: See if it's possible to pass the scriptName via the constructor when creating the instance above
-			b.Initialize(
-				scriptName,
-				new SubConsoleWriter(Console, scriptName)
-			);
-
-			// Create an instance of the script
-			IScript script = b;//.AlignToInterface<IScript>(); // TODO: Remove if not needed
-
-			return script;
+			return ActivateScript(scriptPath);
 		}
 	}
 }
