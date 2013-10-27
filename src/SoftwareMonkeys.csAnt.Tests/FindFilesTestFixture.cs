7f06cf6565260a3586e472a363930341c08661d8
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-27 11:23:40 +1000

    Added test to FindFilesTestFixture.cs

--- src/SoftwareMonkeys.csAnt.Tests/FindFilesTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Tests/FindFilesTestFixture.cs
@@ -59,6 +59,43 @@ namespace SoftwareMonkeys.csAnt.Tests
 			Assert.AreEqual(1, files.Length, "Wrong number of files.");
 		}
 		
+		[Test]
+		public void Test_FindFiles_DoubleStarAndExtension()
+		{
+			var testScript = GetTestScript();
+
+			var patterns = new string[]{
+				"**.txt"
+			};
+
+			var dir = testScript.CurrentDirectory;
+
+			var tmpFile = dir
+				+ Path.DirectorySeparatorChar
+				+ "test"
+				+ Path.DirectorySeparatorChar
+				+ "TestFile.txt";
+
+			var tmpFile2 = dir
+				+ Path.DirectorySeparatorChar
+				+ "test"
+				+ Path.DirectorySeparatorChar
+				+ "TestFile2.log";
+
+			Console.WriteLine ("Tmp file: " + tmpFile);
+			
+			testScript.EnsureDirectoryExists(Path.GetDirectoryName(Path.GetDirectoryName(tmpFile)));
+			testScript.EnsureDirectoryExists(Path.GetDirectoryName(tmpFile));
+
+			File.WriteAllText(tmpFile, "Test content");
+			File.WriteAllText(tmpFile2, "Test content");
+
+			// Should get the file even though it's in a sub directory
+			var files = testScript.FindFiles(dir, patterns);
+
+			Assert.AreEqual(1, files.Length, "Wrong number of files.");
+		}
+		
 		// TODO: Remove if not needed
 		/*[Test]
 		public void Test_FindFiles_StartsWithSlash()
