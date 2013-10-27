81ce6fe6b8273caf8870651c9e4fbc16b535565d
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-27 11:07:07 +1000

    Updated test fixtures

--- src/SoftwareMonkeys.csAnt.Projects.Tests/BuildAllSolutionsTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Projects.Tests/BuildAllSolutionsTestFixture.cs
@@ -1,11 +1,23 @@
 using System;
+using NUnit.Framework;
+using System.IO;
 
 namespace SoftwareMonkeys.csAnt.Projects.Tests
 {
-	public class BuildAllSolutions
+	[TestFixture]
+	public class BuildAllSolutionsTestFixture : BaseProjectsTestFixture
 	{
-		public BuildAllSolutions ()
+		[Test]
+		public void Test_BuildAllSolutions_HandleError()
 		{
+			var script = GetTestScript();
+
+			GrabOriginalScriptingFiles(script);
+
+			script.ExecuteScript("Clean");
+			script.ExecuteScript("BuildAllSolutions");
+
+			Assert.IsTrue (script.IsError, "The IsError flag should be true.");
 		}
 	}
 }
