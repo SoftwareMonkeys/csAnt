e91119dca710d9a976f0e703e57c2cd642256223
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-27 23:53:43 +1000

    Added Initialize.py

---
+++ scripts/Initialize/Initialize.py
@@ -0,0 +1,43 @@
+import os
+import urllib
+
+def Initialize():
+
+	DownloadScripts()
+
+	NextStep()
+
+def NextStep():
+
+	os.system("python " + os.path.abspath("scripts/Initialize/Initialize2.py"))
+
+def DownloadScripts():
+
+	scripts = ['GetCSScript', 'GetHtmlAgilityPack', 'GetLib', 'GetRemainingLibs', 'GetSharpZipLib', 'Initialize2', 'Initialize3', 'RunCSScript', 'Utils']
+
+	for script in scripts:
+
+		print ""
+		print "Script: " + script
+
+	 	scriptPath = os.path.abspath("scripts/Initialize/" + script + ".py")
+		scriptUrl = "https://csant.googlecode.com/git/scripts/Initialize/" + script + ".py"
+
+		print "Script path: " + scriptPath
+		print "Script URL: " + scriptUrl
+	
+		CheckScript(scriptPath,scriptUrl)
+
+		print ""
+
+
+def CheckScript( scriptPath, scriptUrl ):
+	if not os.path.isfile(scriptPath):
+		print "Downloading script"
+		urllib.urlretrieve (scriptUrl, scriptPath)
+	else:
+		
+		print "Skipping download"
+
+Initialize()
+
