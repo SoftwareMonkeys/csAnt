174224e3265d41c9720688185bddeb6156b09e6d
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-27 22:05:58 +1000

    Added RunTestScripts script

---
+++ scripts/RunTestScripts/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("SoftwareMonkeys.csAnt.RunTestScripts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
