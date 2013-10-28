efa4244b2e721b02e24c8d52cd1ce92d056d69d9
CompulsiveCoder <compulsivecoder@gmail.com>
2013-10-28 12:09:22 +1000

    Fixed print function in RunCSScript

--- scripts/Initialize/RunCSScript.py
+++ scripts/Initialize/RunCSScript.py
@@ -16,11 +16,11 @@ def RunCSScript( scriptPath, args ):
         if not (args == ""):
                 command += " " + args
         
-        print ""
-        print "Launching script via cs-script:"
-        print scriptPath
-        print "Command: "
-        print command
-        print ""
+        print("")
+        print("Launching script via cs-script:")
+        print(scriptPath)
+        print("Command:")
+        print(command)
+        print("")
 
         os.system(command)
