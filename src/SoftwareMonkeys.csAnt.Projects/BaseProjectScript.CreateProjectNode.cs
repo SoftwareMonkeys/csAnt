3ccc76124d26f965d753db37c814d05d2355062c
CompulsiveCoder <compulsivecoder@gmail.com>
2013-11-02 09:14:44 +1000

    Added CreateProjectNode

--- src/SoftwareMonkeys.csAnt.Projects/BaseProjectScript.CreateProjectNode.cs
+++ src/SoftwareMonkeys.csAnt.Projects/BaseProjectScript.CreateProjectNode.cs
@@ -1,11 +1,24 @@
 using System;
+using SoftwareMonkeys.FileNodes;
+using System.IO;
 
 namespace SoftwareMonkeys.csAnt.Projects
 {
-	public class BaseProjectScript
+	public partial class BaseProjectScript
 	{
-		public BaseProjectScript ()
+		public void CreateProjectNode(string projectName)
 		{
+			var path = CurrentDirectory
+				+ Path.DirectorySeparatorChar
+				+ projectName
+					+ ".node";
+
+			var node = new FileNode(
+				path,
+				new FileNodeSaver()
+			);
+
+			node.Save();
 		}
 	}
 }
