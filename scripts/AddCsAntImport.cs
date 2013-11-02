7fbb84803e90b7f59510234d800a8a71e987da4e
CompulsiveCoder <compulsivecoder@gmail.com>
2013-11-02 21:24:42 +1000

    Changed default script list in initialize script

---
+++ scripts/AddCsAntImport.cs
@@ -0,0 +1,33 @@
+//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
+//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.dll;
+
+using System;
+using System.IO;
+using Microsoft.CSharp;
+using System.Diagnostics;
+using SoftwareMonkeys.csAnt;
+using SoftwareMonkeys.csAnt.Projects;
+
+class AddCsAntImportScript : BaseProjectScript
+{
+	public static void Main(string[] args)
+	{
+		new AddCsAntImportScript().Start(args);
+	}
+	
+	public override bool Start(string[] args)
+	{
+		Console.WriteLine("");
+		Console.WriteLine("Adding csAnt import...");
+		Console.WriteLine("");
+
+		AddSummary("Added csAnt import info");
+
+                AddImport(
+                        "csAnt",
+                        "https://code.google.com/p/csant/"
+                );
+
+		return !IsError;
+	}
+}
