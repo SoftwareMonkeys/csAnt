c12de21dacbbc9c229c264dd8b99a04ea7c07899
CompulsiveCoder <compulsivecoder@gmail.com>
2013-11-02 16:58:42 +1000

    Fixed initialize scripts

--- scripts/Initialize/GetRemainingLibs.py
+++ scripts/Initialize/GetRemainingLibs.py
@@ -5,18 +5,18 @@ import Utils
 
 def GetRemainingLibs():
 
-	print("")
-	print("----------------------------------------")
-	print("Getting remaining library files by scanning the .node files")
-	print("")
+        print("")
+        print("----------------------------------------")
+        print("Getting remaining library files by scanning the .node files")
+        print("")
 
         if (Utils.IsLinux()):
                 os.system("sh csAnt.sh GetLibs")
         else:
                 os.system("cscript csAnt.vbs GetLibs")
 
-	print("")
-	print("Done")
+        print("")
+        print("Done")
 
-	print("----------------------------------------")
-	print("")
+        print("----------------------------------------")
+        print("")
