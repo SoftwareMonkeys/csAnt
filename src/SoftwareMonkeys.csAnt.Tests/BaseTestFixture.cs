c28fc3d6479c64be4f1d415f8b09462044b2a938
CompulsiveCoder <compulsivecoder@gmail.com>
2013-11-02 10:47:13 +1000

    Added "GetOriginalFiles" functions to BaseTestFixture

--- src/SoftwareMonkeys.csAnt.Tests/BaseTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Tests/BaseTestFixture.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace SoftwareMonkeys.csAnt.Tests
 {
@@ -63,11 +64,6 @@ namespace SoftwareMonkeys.csAnt.Tests
 			Console.WriteLine("Original directory:");
 			Console.WriteLine(script.OriginalDirectory);
 			Console.WriteLine("");
-
-			// TODO: Remove if not needed
-			//testScript.CopyTestFiles(actualProjectDir, tmpDir);
-
-
 		}
 
 		public string GetOriginalDirectory(ITestScript script)
@@ -111,6 +107,61 @@ namespace SoftwareMonkeys.csAnt.Tests
 				Console.WriteLine (destinationFileName);
 
 				File.Copy(file, destinationFileName);
+			}
+		}
+		
+
+		public void GrabOriginalScripts (
+			ITestScript script,
+			params string[] scriptNames
+		)
+		{
+			List<string> list = new List<string> ();
+
+			foreach (var name in scriptNames) {
+				list.Add ("/scripts/" + name + ".cs");
+				list.Add ("/scripts/" + name + "/**");
+			}
+
+			GrabOriginalFiles(
+				script,
+				list.ToArray()
+			);
+		}
+
+		public void GrabOriginalScriptingFiles (ITestScript script)
+		{
+			GrabOriginalFiles(
+				script,
+				"/*.node",
+				"/*.sh",
+				"/lib/**.dll",
+				"/src/**.cs",
+				"/src/**.csproj",
+				"/src/**.sln",
+				"/scripts/**"
+			);
+		}
+
+		public void GrabOriginalFiles (ITestScript script, params string[] patterns)
+		{
+			Console.WriteLine ("");
+			Console.WriteLine ("Grabbing original project files...");
+			Console.WriteLine ("From:");
+			Console.WriteLine (script.OriginalDirectory);
+			Console.WriteLine ("To:");
+			Console.WriteLine (script.CurrentDirectory);
+			Console.WriteLine ("");
+
+			foreach (var file in script.FindFiles (script.OriginalDirectory, patterns)) {
+				Console.WriteLine (file.Replace(script.OriginalDirectory, ""));
+
+				var toFile = file.Replace(script.OriginalDirectory, script.CurrentDirectory);
+
+				if (!Directory.Exists(Path.GetDirectoryName(toFile)))
+					Directory.CreateDirectory(Path.GetDirectoryName(toFile));
+
+				File.Copy (file, toFile);
 			}
 		}
 	}
