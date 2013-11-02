fee1edbeef3f07a55dfb268a1572c5141d8be49f
CompulsiveCoder <compulsivecoder@gmail.com>
2013-11-02 16:31:53 +1100

    Added StartProcess test

--- src/SoftwareMonkeys.csAnt.Tests/StartProcessTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Tests/StartProcessTestFixture.cs
@@ -1,11 +1,41 @@
 using System;
+using NUnit.Framework;
+using System.IO;
 
 namespace SoftwareMonkeys.csAnt.Tests
 {
-	public class StartProcessTestFixture
+	[TestFixture]
+	public class StartProcessTestFixture : BaseTestFixture
 	{
-		public StartProcessTestFixture ()
+		[Test]
+		public void Test_StartProcess()
 		{
+			var script = GetTestScript();
+
+			GrabOriginalFiles(
+				script,
+				"lib/cs-script/**",
+				"lib/csAnt/**",
+				"scripts/HelloWorld.cs"
+			);
+
+			var csscriptExe = script.CurrentDirectory
+				+ Path.DirectorySeparatorChar
+				+ "lib"
+				+ Path.DirectorySeparatorChar
+				+ "cs-script"
+				+ Path.DirectorySeparatorChar
+				+ "cscs.exe";
+
+			var helloWorldScript = script.CurrentDirectory
+				+ Path.DirectorySeparatorChar
+				+ "scripts"
+				+ Path.DirectorySeparatorChar
+					+ "HelloWorld.cs";
+
+			script.StartProcess("mono", csscriptExe, helloWorldScript);
+
+			Assert.IsTrue(script.Console.Output.Contains ("Hello world!"));
 		}
 	}
 }
