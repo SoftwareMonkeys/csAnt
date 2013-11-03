9a665197bbce7813727f860180555e9ef50a79b9
CompulsiveCoder <compulsivecoder@gmail.com>
2013-11-03 10:44:04 +1000

    Tweaked GitClone

--- src/SoftwareMonkeys.csAnt/BaseScript.GitClone.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.GitClone.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 namespace SoftwareMonkeys.csAnt
 {
@@ -24,7 +25,7 @@ namespace SoftwareMonkeys.csAnt
 			Console.WriteLine ("Source: " + sourceDir);
 			Console.WriteLine ("Destination: " + destinationDir);
 
-			var tmpDir = GetTmpDir();
+			var tmpDir = GetTmpDir() + Path.DirectorySeparatorChar + "_clone";
 
 			Git (
 				"clone",
