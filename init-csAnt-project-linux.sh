64d266a8f9495ad8084da3a682d84f27ae108a36
Compulsive Coder <compulsivecoder@gmail.com>
2013-11-27 08:56:38 +1000

    Updated initialize script

--- init-csAnt-project-linux.sh
+++ init-csAnt-project-linux.sh
@@ -1,14 +1,62 @@
-InitScriptFile="/scripts/Initialize/Initialize.py"
-InitScriptFile=$PWD$InitScriptFile
-InitScriptUrl="https://csant.googlecode.com/git/scripts/Initialize/Initialize.py"
+InitScriptFile="scripts/Initialize/Initialize.py"
+InitScriptFileUrl="https://csant.googlecode.com/git/scripts/Initialize/Initialize.py"
 
-if [ ! -f $InitScriptFile ]; then
+BaseDir=$PWD
+ProjectsDir=$BaseDir
 
-	mkdir $PWD"/scripts"
+echo "Current directory:"
+echo $BaseDir
 
-	mkdir $PWD"/scripts/Initialize"
+if [ "$(echo $ProjectsDir | grep '.tmp')" != "" ]; then
+        while true; do
+                     echo $ProjectsDir
+                     
+                     if [ "$(echo $ProjectsDir | grep '.tmp')" = "" ]; then
+                        exit
+                     fi
+                     if [ "$(echo $ProjectsDir | grep '.tmp')" != "" ]; then
+                        ProjectsDir=$(dirname $ProjectsDir)
+                     fi
+        done
+fi
+ProjectsDir=$(dirname $ProjectsDir)
+ProjectsDir=$(dirname $ProjectsDir)
 
-	wget -O $InitScriptFile $InitScriptUrl
+echo "Projects dir:"
+echo $ProjectsDir
+
+InitScriptFileInternal=$BaseDir/$InitScriptFile
+
+CsAntProjectDir="$ProjectsDir/SoftwareMonkeys/csAnt"
+
+InitScriptFileLocal=$CsAntProjectDir/$InitScriptFile
+
+echo "Initialize script file (internal):"
+echo $InitScriptFileInternal
+echo ""
+
+echo "Initialize script file (local):"
+echo $InitScriptFileLocal
+echo ""
+
+echo "Initialize script file (URL):"
+echo $InitScriptFileUrl
+echo ""
+        
+if [ ! -f $InitScriptFileInternal ]; then
+        
+	mkdir "$BaseDir/scripts"
+
+	mkdir "$BaseDir/scripts/Initialize"
+
+        if [ -f $InitScriptFileLocal ]; then
+                echo "Copying initialize script from local location..."
+        
+                cp $InitScriptFileLocal $InitScriptFileInternal
+        else
+                echo "Downloading initialize script..."
+                wget -O $InitScriptFileInternal $InitScriptFileUrl
+        fi
 fi
 
-python3 $InitScriptFile %1 %2 %3
+python3 $InitScriptFileInternal %1 %2 %3 %4 %5
