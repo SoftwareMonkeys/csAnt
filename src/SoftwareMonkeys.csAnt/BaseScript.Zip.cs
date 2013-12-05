1e5b3f31a63dffe7b42da3240c3e2b2f55391e09
Compulsive Coder <compulsivecoder@gmail.com>
2013-12-06 07:26:12 +1000

    Reorganized zip and unzip

--- src/SoftwareMonkeys.csAnt/BaseScript.Zip.cs
+++ src/SoftwareMonkeys.csAnt/BaseScript.Zip.cs
@@ -6,14 +6,6 @@ namespace SoftwareMonkeys.csAnt
 {
 	public partial class BaseScript
 	{
-		public void Zip (string filePattern, string zipFilePath)
-		{
-			Zip (
-				new string[]{filePattern},
-				zipFilePath
-			);
-		}
-
 		/// <summary>
 		/// Zips the files matching the specified file patterns.
 		/// </summary>
@@ -28,113 +20,15 @@ namespace SoftwareMonkeys.csAnt
 		/// <param name='zipFilePath'>
 		/// 
 		/// </param>
-		public void Zip(string[] filePatterns, string zipFilePath)
+		public void Zip(string zipFilePath, params string[] filePatterns)
 		{
-			// TODO: Fix this function so that files outside the CurrentDirectory can be specified. Currently it causes an error
+            var cmd = new ZipCommand(
+                this,
+                zipFilePath,
+                filePatterns
+                );
 
-			Console.WriteLine ("Creating zip file...");
-
-			Console.WriteLine ("  Zip file path: " + zipFilePath);
-
-			if (!Directory.Exists(Path.GetDirectoryName(zipFilePath)))
-				Directory.CreateDirectory(Path.GetDirectoryName(zipFilePath));
-
-			var zipFileName = Path.GetFileNameWithoutExtension(zipFilePath);
-
-			// Zip up the files - From SharpZipLib Demo Code
-           	ZipOutputStream s = new ZipOutputStream(
-				File.Create(zipFilePath)
-			);
-
-			if (IsVerbose)
-				Console.WriteLine ("  Current directory: " + CurrentDirectory);
-            
-            s.SetLevel(9); // 0-9, 9 being the highest compression
-
-            byte[] buffer = new byte[4096];
-
-            foreach (string patternLine in filePatterns)
-            {
-				if (!String.IsNullOrEmpty(patternLine))
-				{
-					Console.WriteLine ("  Specified pattern/file: " + patternLine);
-
-					string pattern = patternLine;
-
-					if (patternLine.IndexOf("|") > -1)
-					{
-						pattern = patternLine.Split('|')[0];
-					}
-
-					string shortPattern = pattern.Replace(CurrentDirectory, "");
-
-					if (IsVerbose)
-						Console.WriteLine ("  Short pattern: " + shortPattern);
-
-					string[] foundFiles = FindFiles(CurrentDirectory, shortPattern);
-					
-					Console.WriteLine ("    Found " + foundFiles.Length.ToString() + " files.");
-					
-					Console.WriteLine ("    Zipping...");
-
-					foreach (string foundFile in foundFiles)
-					{
-						Console.WriteLine ("    " + foundFile.Replace (CurrentDirectory, ""));
-
-						var internalPath = GetZipInternalPath(
-							zipFileName,
-							foundFile,
-							patternLine
-						);
-
-		                ZipEntry entry = new ZipEntry(
-							internalPath
-						);
-
-						entry.DateTime = File.GetLastWriteTime(foundFile);
-
-		                s.PutNextEntry(entry);
-
-		                using (FileStream fs = File.OpenRead(foundFile))
-		                {
-		                    int sourceBytes;
-		                    do
-		                    {
-		                        sourceBytes = fs.Read(buffer, 0,
-		                        buffer.Length);
-
-		                       s.Write(buffer, 0, sourceBytes);
-
-		                    } while (sourceBytes > 0);
-		                }
-					}
-	            }
-			}
-
-            s.Finish();
-            s.Close();
-
-		}
-
-		public string GetZipInternalPath(string zipName, string fileName, string originalPattern)
-		{
-			var subPath = String.Empty;
-
-			if (originalPattern.IndexOf("|") > -1)
-			{
-				var patternParts = originalPattern.Split('|');
-
-				subPath = patternParts[1]
-					+ Path.GetFileName(fileName);
-			}
-			else
-			{
-				subPath = fileName.Replace(CurrentDirectory, "");
-			}
-
-			return zipName
-				+ "/"
-				+ subPath.Trim('/');
+            Execute(cmd);
 		}
 	}
 }
