1a2dcdb2606d7c3de97823692626ea1056a030a2
Compulsive Coder <compulsivecoder@gmail.com>
2014-01-31 19:02:30 +1000

    Updated Test_BuildFromGit test script

--- scripts/Test_BuildFromGit.cs
+++ scripts/Test_BuildFromGit.cs
@@ -4,15 +4,18 @@
 //css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.dll;
 //css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.Tests.dll;
 //css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.Tests.Scripting.dll;
+//css_ref ../lib/csAnt/bin/Release/SetUpFromLocal.exe;
 
 using System;
 using System.IO;
 using Microsoft.CSharp;
 using System.Diagnostics;
 using SoftwareMonkeys.csAnt;
+using SoftwareMonkeys.csAnt.IO;
 using SoftwareMonkeys.csAnt.Projects;
 using SoftwareMonkeys.csAnt.Projects.Tests;
 using SoftwareMonkeys.csAnt.Projects.Tests.Scripting;
+using SoftwareMonkeys.csAnt.SetUpFromLocalConsole;
 
 class Test_BuildFromGitScript : BaseProjectTestScript
 {
@@ -26,22 +29,33 @@ class Test_BuildFromGitScript : BaseProjectTestScript
 		Console.WriteLine("");
 		Console.WriteLine("Test building solutions from git clone...");
 		Console.WriteLine("");
-		
-		FilesGrabber.GrabOriginalFiles();
 
-                // Perform a release cycle to ensure the releases are available for use during initialization
-                ExecuteScript("CycleRelease");
+                var testDir = CurrentDirectory;
+		
+		new FilesGrabber(
+                    OriginalDirectory,
+                    CurrentDirectory
+                ).GrabOriginalFiles();
 
 		// Clone the project to another directory
-		var tmpDir = CloneToTmpDirectory();
+		var dummyProjectDir = CloneToTmpDirectory();
 
-		// Prepare the project (eg. download libs)
-		InstallTo("csAnt", tmpDir);
+		// Relocate to the dummy project directory
+		Relocate(dummyProjectDir);
+
+		// Create the required nodes
+		CreateNodes();
+
+		// Grab the library files (so there's no need to run setup)
+		new FilesGrabber(
+                    testDir,
+                    dummyProjectDir
+               ).GrabOriginalFiles("lib/**");
 
 		if (!IsError)
 		{
 			// Build and test the cloned copy of the project
-			BuildAndTestClonedCopy(tmpDir);
+			BuildClonedCopy(dummyProjectDir);
 		}
 
 		return !IsError;
@@ -51,21 +65,23 @@ class Test_BuildFromGitScript : BaseProjectTestScript
 	{
 		Console.WriteLine("Cloning to tmp directory...");
 
-		var tmpDirectory = GetTmpDir();
+		var projectDirectory = Path.GetDirectoryName(CurrentDirectory)
+                    + Path.DirectorySeparatorChar
+                    + "TestProject";
 
 		Console.WriteLine("Tmp directory:");
-		Console.WriteLine(tmpDirectory);
+		Console.WriteLine(projectDirectory);
 
-		Directory.CreateDirectory(tmpDirectory);
+		Directory.CreateDirectory(projectDirectory);
 
-		GitClone(OriginalDirectory, tmpDirectory);
+		GitClone(OriginalDirectory, projectDirectory);
 
-		return tmpDirectory;
+		return projectDirectory;
 	}
 
-	public void BuildAndTestClonedCopy(string tmpDir)
+	public void BuildClonedCopy(string dummyProjectDir)
 	{
-		CurrentDirectory = tmpDir;
+		CurrentDirectory = dummyProjectDir;
 
 		// Build and test
 		ExecuteScript("CycleBuild");
