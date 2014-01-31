2610d979fb8239f243579b84b7cb4f9e525d60b9
Compulsive Coder <compulsivecoder@gmail.com>
2014-01-31 16:01:05 +1000

    Added ProjectCreator

--- src/SoftwareMonkeys.csAnt.Projects/ProjectCreator.cs
+++ src/SoftwareMonkeys.csAnt.Projects/ProjectCreator.cs
@@ -1,11 +1,31 @@
 using System;
+using System.IO;
 
 namespace SoftwareMonkeys.csAnt.Projects
 {
     public class ProjectCreator
     {
-        public ProjectCreator ()
+        public NodesCreator NodesCreator { get; set; }
+
+        public INodeState NodeState { get;set; }
+
+        public ProjectCreator (
+            INodeState nodeState
+            )
         {
+            NodesCreator = new NodesCreator(nodeState);
+        }
+
+        public void Create (string projectDirectory)
+        {
+            Create (
+                Path.GetFileName(projectDirectory)
+            );
+        }
+
+        public void Create(string projectName, string projectDirectory)
+        {
+            NodesCreator.CreateNodes();
         }
     }
 }
