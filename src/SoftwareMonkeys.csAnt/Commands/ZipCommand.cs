d0ca1a19b67cfb36a8ac71c9f628b958e3eda81c
Compulsive Coder <compulsivecoder@gmail.com>
2014-02-07 12:48:47 +1100

    Tweaked ZipCommand

--- src/SoftwareMonkeys.csAnt/Commands/ZipCommand.cs
+++ src/SoftwareMonkeys.csAnt/Commands/ZipCommand.cs
@@ -23,6 +23,8 @@ namespace SoftwareMonkeys.csAnt
 
         public override void Execute ()
         {
+            // This class is obsolete. Use the FileZipper component
+            
             // TODO: Fix this function so that files outside the CurrentDirectory can be specified. Currently it causes an error
 
             Console.WriteLine ("Creating zip file...");
@@ -35,77 +37,77 @@ namespace SoftwareMonkeys.csAnt
             var zipFileName = Path.GetFileNameWithoutExtension(ZipFilePath);
 
             // Zip up the files - From SharpZipLib Demo Code
-            ZipOutputStream s = new ZipOutputStream(
-                File.Create(ZipFilePath)
-            );
-
-            if (Script.IsVerbose)
-                Console.WriteLine ("  Current directory: " + Script.CurrentDirectory);
-            
-            s.SetLevel(9); // 0-9, 9 being the highest compression
-
-            byte[] buffer = new byte[4096];
-
-            foreach (string patternLine in FilePatterns)
+            using (ZipOutputStream s = new ZipOutputStream(File.Create(ZipFilePath)))
             {
-                if (!String.IsNullOrEmpty(patternLine))
+
+                if (Script.IsVerbose)
+                    Console.WriteLine ("  Current directory: " + Script.CurrentDirectory);
+                
+                s.SetLevel(1); // 0-9, 9 being the highest compression
+    
+                byte[] buffer = new byte[4096];
+    
+                foreach (string patternLine in FilePatterns)
                 {
-                    Console.WriteLine ("  Specified pattern/file: " + patternLine);
-
-                    string pattern = patternLine;
-
-                    if (patternLine.IndexOf("|") > -1)
+                    if (!String.IsNullOrEmpty(patternLine))
                     {
-                        pattern = patternLine.Split('|')[0];
-                    }
-
-                    string shortPattern = pattern.Replace(Script.CurrentDirectory, "");
-
-                    if (Script.IsVerbose)
-                        Console.WriteLine ("  Short pattern: " + shortPattern);
-
-                    string[] foundFiles = Script.FindFiles(Script.CurrentDirectory, shortPattern);
-                    
-                    Console.WriteLine ("    Found " + foundFiles.Length.ToString() + " files.");
-                    
-                    Console.WriteLine ("    Zipping...");
-
-                    foreach (string foundFile in foundFiles)
-                    {
-                        Console.WriteLine ("    " + foundFile.Replace (Script.CurrentDirectory, ""));
-
-                        var internalPath = GetZipInternalPath(
-                            zipFileName,
-                            foundFile,
-                            patternLine
-                        );
-
-                        ZipEntry entry = new ZipEntry(
-                            internalPath
-                        );
-
-                        entry.DateTime = File.GetLastWriteTime(foundFile);
-
-                        s.PutNextEntry(entry);
-
-                        using (FileStream fs = File.OpenRead(foundFile))
+                        Console.WriteLine ("  Specified pattern/file: " + patternLine);
+    
+                        string pattern = patternLine;
+    
+                        if (patternLine.IndexOf("|") > -1)
                         {
-                            int sourceBytes;
-                            do
+                            pattern = patternLine.Split('|')[0];
+                        }
+    
+                        string shortPattern = pattern.Replace(Script.CurrentDirectory, "");
+    
+                        if (Script.IsVerbose)
+                            Console.WriteLine ("  Short pattern: " + shortPattern);
+    
+                        string[] foundFiles = Script.FindFiles(Script.CurrentDirectory, shortPattern);
+                        
+                        Console.WriteLine ("    Found " + foundFiles.Length.ToString() + " files.");
+                        
+                        Console.WriteLine ("    Zipping...");
+    
+                        foreach (string foundFile in foundFiles)
+                        {
+                            Console.WriteLine ("    " + foundFile.Replace (Script.CurrentDirectory, ""));
+    
+                            var internalPath = GetZipInternalPath(
+                                zipFileName,
+                                foundFile,
+                                patternLine
+                            );
+    
+                            ZipEntry entry = new ZipEntry(
+                                internalPath
+                            );
+    
+                            entry.DateTime = File.GetLastWriteTime(foundFile);
+    
+                            s.PutNextEntry(entry);
+    
+                            using (FileStream fs = File.OpenRead(foundFile))
                             {
-                                sourceBytes = fs.Read(buffer, 0,
-                                buffer.Length);
-
-                               s.Write(buffer, 0, sourceBytes);
-
-                            } while (sourceBytes > 0);
+                                int sourceBytes;
+                                do
+                                {
+                                    sourceBytes = fs.Read(buffer, 0,
+                                    buffer.Length);
+    
+                                   s.Write(buffer, 0, sourceBytes);
+    
+                                } while (sourceBytes > 0);
+                            }
                         }
                     }
                 }
+    
+                s.Finish();
+                s.Close();
             }
-
-            s.Finish();
-            s.Close();
         }
         
         public string GetZipInternalPath(string zipName, string fileName, string originalPattern)
