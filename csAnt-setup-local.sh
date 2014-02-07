36be3ea549cf1f36248fe56b0356abd4a5c1d49a
Compulsive Coder <compulsivecoder@gmail.com>
2014-02-07 11:32:52 +1000

    Simplified csAnt-setup-local.sh script

--- csAnt-setup-local.sh
+++ csAnt-setup-local.sh
@@ -6,19 +6,8 @@ if [ -d "$csAntDir" ]; then
     
     echo "Found local csAnt project directory..."
     echo "Copying files..."
-    
-    csAntLibDir=lib/csAnt/bin/Release/
-    
-    if [ ! -d "csAntLibDir" ]; then
-        mkdir lib/
-        mkdir lib/csAnt/
-        mkdir lib/csAnt/bin/
-        mkdir $csAntLibDir
-    fi
         
-	cp $csAntDir/bin/Release/SoftwareMonkeys.csAnt.IO.dll lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.IO.dll
-	cp $csAntDir/bin/Release/SoftwareMonkeys.csAnt.IO.Contracts.dll lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.IO.Contracts.dll
-	cp $csAntDir/bin/Release/SetUpFromLocal.exe lib/csAnt/bin/Release/SetUpFromLocal.exe
+    cp $csAntDir/bin/Release/Packed/SetUpFromLocal.exe csAnt-SetUpFromLocal.exe
 
-	mono lib/csAnt/bin/Release/SetUpFromLocal.exe
- fi
+    mono csAnt-SetUpFromLocal.exe
+fi
