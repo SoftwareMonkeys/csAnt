722a0b7328ac52590a037a89d72e9aea818b4ebc
Compulsive Coder <compulsivecoder@gmail.com>
2014-03-03 12:10:51 +1000

    Fixed SetUpFromWebConsole assembly info and project details

--- src/SoftwareMonkeys.csAnt.SetUpFromWebConsole/AssemblyInfo.cs
+++ src/SoftwareMonkeys.csAnt.SetUpFromWebConsole/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("SoftwareMonkeys.csAnt.SetUpFromWebConsole.cs")]
+[assembly: AssemblyTitle("SoftwareMonkeys.csAnt.SetUpFromWebConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
