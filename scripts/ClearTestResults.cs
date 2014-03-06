9d300fd4e845e4303c5af246166cba474d3be72c
Compulsive Coder <compulsivecoder@gmail.com>
2014-03-06 11:44:39 +1000

    Removed unneeded references in scripts

--- scripts/ClearTestResults.cs
+++ scripts/ClearTestResults.cs
@@ -1,6 +1,3 @@
-//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.dll;
-//css_ref ../lib/csAnt/bin/Release/SoftwareMonkeys.csAnt.Projects.dll;
-
 using System;
 using System.IO;
 using Microsoft.CSharp;
