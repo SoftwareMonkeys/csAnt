df7c25416b37242357b23cf05b373e5e59645a7f
Compulsive Coder <compulsivecoder@gmail.com>
2014-03-07 14:31:19 +1000

    Tweaked AssemblyInfo

--- src/SoftwareMonkeys.csAnt.IO/AssemblyInfo.cs
+++ src/SoftwareMonkeys.csAnt.IO/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("john")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
