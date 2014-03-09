6a48b93b07bbb706a3893e60d7b4a38eb236045e
Compulsive Coder <compulsivecoder@gmail.com>
2014-03-09 13:33:29 +1000

    Removed unneeded reference from csAntConsole Program.cs

--- src/SoftwareMonkeys.csAnt.UI.csAntConsole/Program.cs
+++ src/SoftwareMonkeys.csAnt.UI.csAntConsole/Program.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using SoftwareMonkeys.csAnt.Packages;
 using SoftwareMonkeys.csAnt.IO;
 
 namespace SoftwareMonkeys.csAnt.UI.csAntConsole
