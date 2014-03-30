882d968ea0fcf6fef5108be1d0358f0eac0b3de4
Compulsive Coder <compulsivecoder@gmail.com>
2014-03-31 09:12:59 +1000

    Overhauled setup to use nuget

--- src/SoftwareMonkeys.csAnt.SetUpFromWebConsole/Installer.cs
+++ src/SoftwareMonkeys.csAnt.SetUpFromWebConsole/Installer.cs
@@ -1,10 +1,109 @@
 using System;
+using SoftwareMonkeys.csAnt.External.Nuget;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SoftwareMonkeys.csAnt.IO;
+
+
 namespace SoftwareMonkeys.csAnt.SetUpFromWebConsole
 {
+    // TODO: Tidy up the code in this class
     public class Installer
     {
+        public IFileFinder FileFinder { get;set; }
+
         public Installer ()
         {
+            FileFinder = new FileFinder();
+        }
+
+        public void Install(bool overwrite)
+        {
+            InstallNuget();
+
+            // TODO: Move this to a config file
+            var feedPath = "https://www.myget.org/F/csant/";
+
+            // TODO: Move this to a config file
+            var outputDir = "lib";
+
+            // TODO: Move the executor to a property
+            var nugetExecutor = new NugetExecutor();
+            nugetExecutor.Execute(
+                "install",
+                "csAnt",
+                "-OutputDirectory " + outputDir,
+                "-Source " + feedPath
+            );
+
+            DeployFiles(overwrite);
+        }
+
+        public void InstallNuget()
+        {
+            // TODO: Move the nuget checker to a property
+            var nugetChecker = new NugetChecker();
+            nugetChecker.CheckNuget();
+        }
+
+        public void DeployFiles(bool overwrite)
+        {
+            DeployGeneralFiles(overwrite);
+
+            DeployLibFiles(overwrite);
+        }
+
+        public void DeployGeneralFiles(bool overwrite)
+        {
+            var files = new string[]{
+                "csAnt.sh",
+                "csAnt.bat",
+                "scripts/*"
+            };
+
+            var libDir = Path.Combine(Environment.CurrentDirectory, "lib");
+
+            var directory = GetVersionedLibDir(libDir);
+
+            foreach (var file in FileFinder.FindFiles(directory, files))
+            {
+                var toFile = file.Replace(directory, Environment.CurrentDirectory);
+
+                if (!Directory.Exists(Path.GetDirectoryName(toFile)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(toFile));
+
+                if (overwrite || !File.Exists(toFile))
+                    File.Copy(file, toFile, overwrite);
+            }
+        }
+
+        public void DeployLibFiles(bool overwrite)
+        {
+            var libDir = Path.Combine(Environment.CurrentDirectory, "lib");
+
+            var versionedDir = GetVersionedLibDir(libDir);
+
+            var versiondSubDir = versionedDir
+                + Path.DirectorySeparatorChar
+                + "lib"
+                + Path.DirectorySeparatorChar
+                + "csAnt";
+
+            var generalDir = Path.Combine(
+                libDir,
+                "csAnt"
+            );
+
+            // Move from "lib/csAnt.1.2.3.4/lib/csAnt/" to just "/lib/csAnt/"
+            Directory.Move(versiondSubDir, generalDir);
+        }
+
+        public string GetVersionedLibDir(string libDir)
+        {
+            return new List<DirectoryInfo>(
+                new DirectoryInfo(libDir).GetDirectories().OrderByDescending(p => p.CreationTime)
+            )[0].FullName;
         }
     }
 }
--- src/SoftwareMonkeys.csAnt.SetUpFromWebConsole/Main.cs
+++ src/SoftwareMonkeys.csAnt.SetUpFromWebConsole/Main.cs
@@ -5,15 +5,17 @@ using HtmlAgilityPack;
 using System.Diagnostics;
 using SoftwareMonkeys.csAnt.IO.Compression;
 using SoftwareMonkeys.csAnt.IO;
+using System.Linq;
+using SoftwareMonkeys.csAnt.External.Nuget;
 
-namespace SoftwareMonkeys.csAnt.SetUpFromWebConsole.cs
+namespace SoftwareMonkeys.csAnt.SetUpFromWebConsole
 {
     class MainClass
     {
         public static void Main (string[] args)
         {
             Console.WriteLine ("");
-            Console.WriteLine ("Setting up csAnt from online files...");
+            Console.WriteLine ("Setting up csAnt...");
             Console.WriteLine ("");
 
             // TODO: Clean up and reorganise code
@@ -25,6 +27,10 @@ namespace SoftwareMonkeys.csAnt.SetUpFromWebConsole.cs
                 destinationPath = Path.GetFullPath(arguments["d"]);
 
             var overwrite = arguments.Contains("o");
+            
+            var showIntro = true;
+            if (arguments.Contains("i"))
+                showIntro = Convert.ToBoolean(arguments["i"]);
 
             var releaseName = "standard";
 
@@ -34,13 +40,16 @@ namespace SoftwareMonkeys.csAnt.SetUpFromWebConsole.cs
             if (releaseName.IndexOf("-release") > -1)
                 releaseName = releaseName.Replace("-release", "");
 
-            var url = GetUrl ("csAnt-" + releaseName + "-release");
+            Install(overwrite);
 
-            Console.WriteLine (url);
+            if (showIntro)
+                ShowIntro();
+        }
 
-            DownloadReleaseZip(url, overwrite);
-
-            ShowIntro();
+        // TODO: Move this code into a dedicated installer component
+        static public void Install(bool overwrite)
+        {
+            new Installer().Install(overwrite);
         }
 
         static public void ShowIntro()
@@ -79,97 +88,6 @@ namespace SoftwareMonkeys.csAnt.SetUpFromWebConsole.cs
             Console.WriteLine ("3) Launch your script:");
             Console.WriteLine ("  {0} [YourScriptName]", prefix);
             Console.WriteLine ("");
-        }
-
-        static public void DownloadReleaseZip(string url, bool overwrite)
-        {
-            var zipFile = Environment.CurrentDirectory
-                + Path.DirectorySeparatorChar
-                    + "csAnt.zip";
-
-            new FileDownloader().Download(url, zipFile, overwrite);
-
-            var zipper = new FileZipper();
-
-            var unzippedPath = Environment.CurrentDirectory
-                + Path.DirectorySeparatorChar
-                    + "_csAnt-Unzipped";
-
-            zipper.Unzip(zipFile, unzippedPath, "/");
-
-            InstallFilesFromRelease(unzippedPath, overwrite);
-        }
-
-        static public void InstallFilesFromRelease(string unzippedPath, bool overwrite)
-        {
-            Console.WriteLine ("");
-            Console.WriteLine ("Installing files:");
-
-            foreach (var file in Directory.GetFiles (unzippedPath, "*", SearchOption.AllDirectories))
-            {
-                var toFile = file.Replace(unzippedPath, Environment.CurrentDirectory);
-
-                if (!File.Exists (toFile) || overwrite)
-                {
-                    File.Copy(file, toFile);
-
-                    Console.WriteLine (toFile.Replace(Environment.CurrentDirectory, ""));
-                }
-                else
-                {
-                    Console.WriteLine ("Skipped: " + toFile.Replace(Environment.CurrentDirectory, ""));
-                }
-            }
-            Console.WriteLine ("");
-        }
-
-        static public string GetUrl(string key)
-        {
-            var url = "https://code.google.com/p/csant/downloads/list";
-    
-            var xpath = "//table[@id='resultstable']/tr/td[3]";
-    
-            var prefix = "https://csant.googlecode.com/files/";
-    
-            var data = ScrapeXPathArray(
-                url,
-                xpath
-            );
-    
-            foreach (string item in data)
-            {
-                if (item.IndexOf(key + "-") == 0)
-                {    
-                    return prefix + item;
-                }
-            }
-    
-            return String.Empty;
-        }
-        
-        static public string[] ScrapeXPathArray(
-            string url,
-            string xpath
-        )
-        {
-            var web = new HtmlWeb();
-    
-            var doc = web.Load(url);
-    
-            var nodes = doc.DocumentNode.SelectNodes(xpath);
-    
-            List<string> values = new List<string>();
-    
-            if (nodes != null)
-            {    
-                foreach (var node in nodes)
-                {
-                    if (!String.IsNullOrEmpty(node.InnerText.Trim ()))
-                        values.Add (node.InnerText.Trim ());
-                }
-            }
-    
-            return values.ToArray();
         }
     }
 }
