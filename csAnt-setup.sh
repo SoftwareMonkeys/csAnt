53a2bc733185042cc8e521b557b26a689cb1f4b4
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-02 10:02:43 +1000

    Updated setup script

--- csAnt-setup.sh
+++ csAnt-setup.sh
@@ -1,2 +1,18 @@
-wget "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=csant&DownloadId=812860&FileTime=130391427469870000&Build=20885" -O csAnt-SetUp.exe
-mono csAnt-SetUp.exe $@
+# Get nuget
+mkdir lib
+wget "http://nuget.org/nuget.exe" -O lib/nuget.exe
+
+# Get csAnt setup package
+mono lib/nuget.exe install csAnt-setup -Source https://www.myget.org/F/csant/ -OutputDirectory lib
+
+# Enter csAnt package dir
+cd lib/csAnt-setup.*
+
+# Move the setup file back to the root
+cp csAnt-SetUp.exe ../../
+
+# Return to the root dir
+cd ../../
+
+# Run the setup file
+mono csAnt-SetUp.exe
