a86cad901e3f301d22a4dcf2e030f8fd309204a0
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-03 12:37:17 +1000

    Added CommitVersion script

---
+++ scripts/CommitVersion.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Diagnostics;
+using SoftwareMonkeys.csAnt;
+using SoftwareMonkeys.csAnt.Projects;
+
+class CommitAssemblyInfoFilesScript : BaseProjectScript
+{
+	public static void Main(string[] args)
+	{
+		new CommitAssemblyInfoFilesScript().Start(args);
+	}
+	
+	public override bool Run(string[] args)
+	{
+        var version = CurrentNode.Properties["Version"];
+
+        var message = @"""Updated version: " + version + @"""";
+
+        var patterns = new string[]{
+            "*.node",
+            "src/**.node",
+            "**AssemblyInfo.cs"       
+        };
+
+        Git(
+            "reset"
+        );
+
+        foreach (var file in FindFiles(patterns))
+        {
+            Git(
+                "add",
+                ToRelative(file)
+            );
+        }
+
+        Git(
+            "commit",
+            "*",
+            "-m " + message
+        );
+
+        Git(
+            "push",
+            "origin",
+            "master"
+        );
+    
+        return !IsError;
+	}
+}
