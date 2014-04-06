261b9f464dfea1b8476b855f5e29077de68c3f39
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-06 10:09:32 +1000

    Added PathConverter

--- src/SoftwareMonkeys.csAnt.IO/PathConverter.cs
+++ src/SoftwareMonkeys.csAnt.IO/PathConverter.cs
@@ -1,10 +1,46 @@
 using System;
+using System.IO;
+
+
 namespace SoftwareMonkeys.csAnt.IO
 {
     public class PathConverter
     {
-        public PathConverter ()
+        static public string ToAbsolute (string relativePath)
         {
+            if (!IsAbsolute (relativePath)) {
+                return Path.GetFullPath (relativePath);
+            } else {
+                var absolutePath = relativePath;
+                return absolutePath;
+            }
+        }
+        
+        static public string ToAbsolute (string basePath, string relativePath)
+        {
+            if (!IsAbsolute (relativePath)) {
+                return basePath.TrimEnd(Path.DirectorySeparatorChar)
+                    + Path.DirectorySeparatorChar
+                        + relativePath.TrimStart(Path.DirectorySeparatorChar);
+            } else {
+                var absolutePath = relativePath;
+                return absolutePath;
+            }
+        }
+
+        static public string ToRelative(string absolutePath)
+        {
+            return absolutePath.Replace(Environment.CurrentDirectory, "").TrimStart('/').TrimStart('\\');
+        }
+
+        static public bool IsAbsolute(string path)
+        {
+            if (Platform.IsWindows) {
+                return path.Contains (":");
+            } else {
+                // TODO: Check if this is the best way of doing this
+                return path.StartsWith (Path.DirectorySeparatorChar.ToString());
+            }
         }
     }
 }
