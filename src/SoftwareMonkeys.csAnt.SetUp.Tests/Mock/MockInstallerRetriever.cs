4a7f2aba8f7710ea9e259d7bd5877064c8abc86c
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-06 10:34:18 +1000

    Updated MockInstallerRetriever

--- src/SoftwareMonkeys.csAnt.SetUp.Tests/Mock/MockInstallerRetriever.cs
+++ src/SoftwareMonkeys.csAnt.SetUp.Tests/Mock/MockInstallerRetriever.cs
@@ -1,14 +1,38 @@
 using System;
-namespace SoftwareMonkeys.csAnt.SetUp.Common.Tests
+using System.IO;
+using SoftwareMonkeys.csAnt.IO;
+
+
+namespace SoftwareMonkeys.csAnt.SetUp.Tests
 {
-    public class MockInstallerNugetRetriever : InstallerNugetRetriever
+    public class MockInstallerRetriever : BaseInstallerRetriever
     {
-        public MockInstallerNugetRetriever(string source, string destination, Version version)
-            : base(
-                new MockNugetChecker(false),
-                new MockNugetExecutor(source, destination, version)
-                )
+        public string SourcePath { get;set; }
+
+        public string DestinationPath { get;set; }
+
+        public Version Version { get;set; }
+
+        public MockInstallerRetriever(string source, string destination, Version version)
         {
+            SourcePath = source;
+            DestinationPath = destination;
+            Version = version;
+        }
+
+        public override void Retrieve ()
+        {
+            var toDir = DestinationPath
+                + Path.DirectorySeparatorChar
+                    + "lib"
+                    + Path.DirectorySeparatorChar
+                    + "csAnt."
+                    + Version;
+
+            new FilesGrabber(
+                SourcePath,
+                toDir
+                ).GrabOriginalFiles();
         }
     }
 }
