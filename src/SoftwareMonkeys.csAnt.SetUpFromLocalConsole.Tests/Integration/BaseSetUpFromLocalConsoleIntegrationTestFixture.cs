85faaba13bae701f546a4b176d4da5b66f302dc8
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-11 14:46:41 +1000

    Fixed IO.Tests.csproj

---
+++ src/SoftwareMonkeys.csAnt.SetUpFromLocalConsole.Tests/Integration/BaseSetUpFromLocalConsoleIntegrationTestFixture.cs
@@ -0,0 +1,14 @@
+using System;
+using SoftwareMonkeys.csAnt.Tests;
+
+
+namespace SoftwareMonkeys.csAnt.SetUpFromLocalConsole.Tests.Integration
+{
+    public class BaseSetUpFromLocalConsoleIntegrationTestFixture : BaseIntegrationTestFixture
+    {
+        public BaseSetUpFromLocalConsoleIntegrationTestFixture ()
+        {
+        }
+    }
+}
+
---
+++ src/SoftwareMonkeys.csAnt.SetUpFromLocalConsole.Tests/Unit/BaseSetUpFromLocalConsoleIntegrationTestFixture.cs
@@ -0,0 +1,14 @@
+using System;
+using SoftwareMonkeys.csAnt.Tests;
+
+
+namespace SoftwareMonkeys.csAnt.SetUpFromLocalConsole.Tests
+{
+    public class BaseSetUpFromLocalConsoleUnitTestFixture : BaseUnitTestFixture
+    {
+        public BaseSetUpFromLocalConsoleUnitTestFixture ()
+        {
+        }
+    }
+}
+
