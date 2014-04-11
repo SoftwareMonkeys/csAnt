13f97418cc8eb38d89fccd5d46430c8006b993ff
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-11 14:49:32 +1000

    Fixed references in FilesGrabber

--- src/SoftwareMonkeys.csAnt.IO/FilesGrabber.cs
+++ src/SoftwareMonkeys.csAnt.IO/FilesGrabber.cs
@@ -23,14 +23,14 @@ namespace SoftwareMonkeys.csAnt.IO
                     "csAnt-SetUpFromLocal.exe",
                     "lib/nuget.exe",
                     "lib/csAnt/**",
-                    "lib/FileNodes/**",
+                    "lib/FileNodes.0.5.0.0/**",
                     "lib/HtmlAgilityPack.1.4.6/lib/Net40/**",
                     "lib/CS-Script.3.7.2.0/lib/net40/**",
                     "lib/SharpZipLib.0.86.0/lib/20/**",
                     "lib/ILRepack.1.25.0/**",
                     "lib/Newtonsoft.Json.6.0.2/lib/net40/**",
-                    "lib/NUnit.2.6.3/lib/nunit.framework.dll",
-                    "lib/NUnit.Runners.2.6.3/tools/nunit-console.exe",
+                    "lib/NUnit.2.6.0.12051/lib/nunit.framework.dll",
+                    "lib/NUnit.Runners.2.6.0.12051/tools/nunit-console.exe",
                     "lib/NUnitResults.1.1/*"
                 };
             }
@@ -53,7 +53,8 @@ namespace SoftwareMonkeys.csAnt.IO
             get
             {
                 return new string[] {
-                    "bin/**"
+                    "bin/*",
+                    "bin/packed/*"
                 };
             }
         }
