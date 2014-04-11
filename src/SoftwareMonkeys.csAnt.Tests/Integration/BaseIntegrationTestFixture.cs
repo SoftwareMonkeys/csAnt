bbe6df965df8c4a5bb58021d0cd4f3ba10ded386
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-11 14:57:58 +1000

    Updated BaseIntegrationTestFixture

--- src/SoftwareMonkeys.csAnt.Tests/Integration/BaseIntegrationTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Tests/Integration/BaseIntegrationTestFixture.cs
@@ -1,6 +1,10 @@
 using System;
+using NUnit.Framework;
+
+
 namespace SoftwareMonkeys.csAnt.Tests
 {
+    [Category("Integration")]
     public class BaseIntegrationTestFixture : BaseTestFixture
     {
         public BaseIntegrationTestFixture ()
