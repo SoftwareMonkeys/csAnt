83bfb9a1a59950acb62843db14dcd2567235b78c
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-11 14:53:47 +1000

    Updated Repack-SetUpFromLocal

--- scripts/Repack-SetUpFromLocal.cs
+++ scripts/Repack-SetUpFromLocal.cs
@@ -21,7 +21,9 @@ class Repack_SetUpFromLocalScript : BaseScript
                 buildMode = arguments["mode"];
 
             var dependencies = new string[]{
+                "bin/{BuildMode}/SoftwareMonkeys.FileNodes.dll",
                 "bin/{BuildMode}/SoftwareMonkeys.csAnt.dll",
+                "bin/{BuildMode}/SoftwareMonkeys.csAnt.Contracts.dll",
                 "bin/{BuildMode}/SoftwareMonkeys.csAnt.External.Nuget.dll",
                 "bin/{BuildMode}/SoftwareMonkeys.csAnt.IO.dll",
                 "bin/{BuildMode}/SoftwareMonkeys.csAnt.IO.Contracts.dll",
