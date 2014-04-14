33fe91b45460f698dd9d09f85dd739e4cd8f1a7c
Compulsive Coder <compulsivecoder@gmail.com>
2014-04-14 13:45:50 +1000

    Added BaseLiveTestScript.cs

---
+++ src/SoftwareMonkeys.csAnt.Tests.Scripting/Live/BaseLiveTestScript.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+
+namespace SoftwareMonkeys.csAnt.Tests.Scripting.Live
+{
+    [Category("Live")]
+    abstract public class BaseLiveTestScript : BaseTestScript
+    {
+        public BaseLiveTestScript ()
+        {
+        }
+    }
+}
+
