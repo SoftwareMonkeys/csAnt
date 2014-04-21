b926f034a9339553e26304ece4220ba4145d04c1
CompulsiveCoder <compulsivecoder@gmail.com>
2014-04-21 11:29:19 +1000

    Updated Arguments.cs

--- src/SoftwareMonkeys.csAnt/Arguments.cs
+++ src/SoftwareMonkeys.csAnt/Arguments.cs
@@ -10,13 +10,13 @@ using System.Collections;
 
 namespace SoftwareMonkeys.csAnt
 {
-	/// <summary>
-	/// Arguments class
-	/// </summary>
-	public class Arguments
-	{
-		// Variables
-		private StringDictionary Parameters;
+    /// <summary>
+    /// Arguments class
+    /// </summary>
+    public class Arguments
+    {
+        // Variables
+        private StringDictionary Parameters;
 
         public string[] KeylessArguments = new string[]{};
 
@@ -25,108 +25,109 @@ namespace SoftwareMonkeys.csAnt
             get { return Parameters.Count; }
         }
 
-		// Constructor
-		public Arguments(params string[] args)
-		{
-		    Parameters = new StringDictionary();
-		    Regex splitter = new Regex(@"^-{1,2}|^/|=|:",
-			RegexOptions.IgnoreCase|RegexOptions.Compiled);
+        // Constructor
+        public Arguments(params string[] args)
+        {
+            Parameters = new StringDictionary();
+            Regex splitter = new Regex(@"^-{1,2}|^/|=|:",
+            RegexOptions.IgnoreCase|RegexOptions.Compiled);
 
-		    Regex remover = new Regex(@"^['""]?(.*?)['""]?$",
-			RegexOptions.IgnoreCase|RegexOptions.Compiled);
+            Regex remover = new Regex(@"^['""]?(.*?)['""]?$",
+            RegexOptions.IgnoreCase|RegexOptions.Compiled);
 
-		    string parameter = null;
-		    string[] parts;
+            string parameter = null;
+            string[] parts;
 
             var keyless = new List<string>();
 
-		    // Valid parameters forms:
-		    // {-,/,--}param{ ,=,:}((",')value(",'))
-		    // Examples: 
-		    // -param1 value1 --param2 /param3:"Test-:-work" 
-		    //   /param4=happy -param5 '--=nice=--'
-		    foreach(string arg in args)
-		    {
+            // Valid parameters forms:
+            // {-,/,--}param{ ,=,:}((",')value(",'))
+            // Examples: 
+            // -param1 value1 --param2 /param3:"Test-:-work" 
+            //   /param4=happy -param5 '--=nice=--'
+            foreach(string arg in args)
+            {
                 if (!arg.Contains(":")
-                    && !arg.Contains("="))
+                    && !arg.Contains("=")
+                    && !arg.StartsWith("-"))
                     keyless.Add(arg);
 
-			// Look for new parameters (-,/ or --) and a
-			// possible enclosed value (=,:)
-    			parts = splitter.Split(arg,3);
+            // Look for new parameters (-,/ or --) and a
+            // possible enclosed value (=,:)
+                parts = splitter.Split(arg,3);
     
-    			switch(parts.Length){
-    			// Found a value (for the last parameter 
-    			// found (space separator))
-    			case 1:
-    			    if(parameter != null)
-    			    {
-    				if(!Parameters.ContainsKey(parameter)) 
-    				{
-    				    parts[0] = 
-    				        remover.Replace(parts[0], "$1");
+                switch(parts.Length){
+                // Found a value (for the last parameter 
+                // found (space separator))
+                case 1:
+                    if(parameter != null)
+                    {
+                    if(!Parameters.ContainsKey(parameter)) 
+                    {
+                        parts[0] = 
+                            remover.Replace(parts[0], "$1");
     
-    				    Parameters.Add(parameter, parts[0]);
-    				}
-    				parameter=null;
-    			    }
-    			    // else Error: no parameter waiting for a value (skipped)
-    			    break;
+                        Parameters.Add(parameter, parts[0]);
+                    }
+                    parameter=null;
+                    }
+                    // else Error: no parameter waiting for a value (skipped)
+                    break;
     
-    			// Found just a parameter
-    			case 2:
-    			    // The last parameter is still waiting. 
-    			    // With no value, set it to true.
-    			    if(parameter!=null)
-    			    {
-    				if(!Parameters.ContainsKey(parameter)) 
-    				    Parameters.Add(parameter, "true");
-    			    }
-    			    parameter=parts[1];
-    			    break;
+                // Found just a parameter
+                case 2:
+                    // The last parameter is still waiting. 
+                    // With no value, set it to true.
+                    if(parameter!=null)
+                    {
+                    if(!Parameters.ContainsKey(parameter)) 
+                        Parameters.Add(parameter, "true");
+                    }
+                    parameter=parts[1];
+                    break;
     
-    			// Parameter with enclosed value
-    			case 3:
-    			    // The last parameter is still waiting. 
-    			    // With no value, set it to true.
-    			    if(parameter != null)
-    			    {
-        				if(!Parameters.ContainsKey(parameter)) 
-        				    Parameters.Add(parameter, "true");
-    			    }
+                // Parameter with enclosed value
+                case 3:
+                    // The last parameter is still waiting. 
+                    // With no value, set it to true.
+                    if(parameter != null)
+                    {
+                        if(!Parameters.ContainsKey(parameter)) 
+                            Parameters.Add(parameter, "true");
+                    }
     
-    			    parameter = parts[1];
+                    parameter = parts[1];
     
-    			    // Remove possible enclosing characters (",')
-    			    if(!Parameters.ContainsKey(parameter))
-    			    {
-        				parts[2] = remover.Replace(parts[2], "$1");
-        				Parameters.Add(parameter, parts[2]);
-    			    }
+                    // Remove possible enclosing characters (",')
+                    if(!Parameters.ContainsKey(parameter))
+                    {
+                        parts[2] = remover.Replace(parts[2], "$1");
+                        Parameters.Add(parameter, parts[2]);
+                    }
     
-    			    parameter=null;
-    			    break;
-    			}
-		    }
-		    // In case a parameter is still waiting
-		    if(parameter != null)
-		    {
-			    if(!Parameters.ContainsKey(parameter)) 
-			        Parameters.Add(parameter, "true");
-		    }
+                    parameter=null;
+                    break;
+                }
+            }
+            // In case a parameter is still waiting
+            if(parameter != null)
+            {
+                if(!Parameters.ContainsKey(parameter)) 
+                    Parameters.Add(parameter, "true");
+            }
 
             KeylessArguments = keyless.ToArray();
-		}
+        }
 
-		// Retrieve a parameter value if it exists 
-		// (overriding C# indexer property)
-		public string this [string param]
-		{
-		    get
-		    {
-			return(Parameters[param]);
-		    }
-		}
+        // Retrieve a parameter value if it exists 
+        // (overriding C# indexer property)
+        public string this [string param]
+        {
+            get
+            {
+            return(Parameters[param]);
+            }
+        }
 
         public string this [params string[] parameters]
         {
@@ -140,22 +141,25 @@ namespace SoftwareMonkeys.csAnt
             }
         }
 
-		public bool Contains(string param)
-		{
-			return Parameters.ContainsKey(param);
-		}
+        public bool Contains(string param)
+        {
+            return Parameters.ContainsKey(param);
+        }
 
         public override string ToString ()
         {
             var builder = new StringBuilder();
 
-            int i = 0;
+            foreach (var value in KeylessArguments)
+            {
+                builder.Append (value);
+                builder.Append (" ");
+            }
+
             foreach (DictionaryEntry entry in Parameters)
             {
                 builder.Append (entry.Value);
-                if (i < this.Count-1)
-                    builder.Append(" ");
-                i++;
+                builder.Append (" ");
             }
 
             return builder.ToString().Trim();
@@ -173,8 +177,7 @@ namespace SoftwareMonkeys.csAnt
 
             return doesContain;
         }
-	}
+    }
 }
-
 
 
