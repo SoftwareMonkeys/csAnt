1e6fdd273823046a79f788e38eb5e782c8581502
CompulsiveCoder <compulsivecoder@gmail.com>
2014-04-23 13:19:29 +1000

    Updated version: 0.5.8.799

--- src/SoftwareMonkeys.csAnt.External.Nuget.Tests/AssemblyInfo.cs
+++ src/SoftwareMonkeys.csAnt.External.Nuget.Tests/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.4.52.100")]
+[assembly: AssemblyVersion("0.5.8.699")]
