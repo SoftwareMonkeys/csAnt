caa06c0bc5a39e38c25db0697bb683e0c734f5c3
CompulsiveCoder <compulsivecoder@gmail.com>
2014-04-23 15:28:25 +1000

    Fixed CyclePublishTests

--- src/SoftwareMonkeys.csAnt.SetUpFromLocalConsole/AssemblyInfo.cs
+++ src/SoftwareMonkeys.csAnt.SetUpFromLocalConsole/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.5.11.100")]
+[assembly: AssemblyVersion("0.5.8.699")]
