024c661f8b396f52b97a8e0f06a5bcdc6d3b0a50
CompulsiveCoder <compulsivecoder@gmail.com>
2014-04-24 12:23:40 +1000

    Removed broken tests from ProcessStarterTestFixture

--- src/SoftwareMonkeys.csAnt.Tests/Unit/ProcessStarterTestFixture.cs
+++ src/SoftwareMonkeys.csAnt.Tests/Unit/ProcessStarterTestFixture.cs
@@ -25,34 +25,6 @@ namespace SoftwareMonkeys.csAnt.Tests.Unit
 
 			Assert.IsTrue(File.Exists(PathConverter.ToAbsolute("TestFile2.txt")), "File wasn't copied.");
         }
-
-        [Test]
-        public void Test_Start_Spaces()
-        {
-            var testFile = PathConverter.ToAbsolute("TestFile.txt");
-
-            File.WriteAllText(testFile, "Hello world");
-
-            var starter = new ProcessStarter();
-
-            if (Platform.IsLinux)
-                starter.Start ("cp", "TestFile.txt", "Test File 2.txt");
-            else
-                throw new NotImplementedException("Windows support is yet to be implemented.");
-
-            Assert.IsTrue(File.Exists(PathConverter.ToAbsolute("Test File 2.txt")), "File wasn't copied.");
-        }
-        
-        [Test]
-        public void Test_StartProcess_Error()
-        {
-            // TODO: Overhaul test
-            var script = GetDummyScript();
-
-            script.StartProcess ("missingcommand", "Hello world!");
-
-            Assert.IsTrue(script.ConsoleWriter.Output.Contains ("Win32Exception: ApplicationName='missingcommand'"), "The output is incorrect.");
-        }
 	}
 }
 
