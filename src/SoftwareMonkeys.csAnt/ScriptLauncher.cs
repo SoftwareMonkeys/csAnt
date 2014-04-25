95e9619599dc3da952727d03775ec845eabf6f1c
CompulsiveCoder <compulsivecoder@gmail.com>
2014-04-25 10:33:57 +1000

    Added ScriptLauncher

--- src/SoftwareMonkeys.csAnt/ScriptLauncher.cs
+++ src/SoftwareMonkeys.csAnt/ScriptLauncher.cs
@@ -1,11 +1,28 @@
 using System;
+using SoftwareMonkeys.csAnt.Processes;
 
 namespace SoftwareMonkeys.csAnt
 {
+    /// <summary>
+    /// Launches scripts via the standard launcher file, which executes the csAnt.exe file and starts the script.
+    /// Linux: sh csAnt.sh [ScriptName]
+    /// Windows: csAnt.bat [ScriptName]
+    /// </summary>
     public class ScriptLauncher
     {
+        public ProcessStarter Starter { get;set; }
+
         public ScriptLauncher ()
         {
+            Starter = new ProcessStarter();
+        }
+
+        public void Launch(string scriptName, params string[] arguments)
+        {
+            if (Platform.IsLinux)
+                Starter.Start("sh", "csAnt.sh", "RaiseEvent", "Install");
+            else
+                Starter.Start("csAnt.bat", "RaiseEvent", "Install");
         }
     }
 }
