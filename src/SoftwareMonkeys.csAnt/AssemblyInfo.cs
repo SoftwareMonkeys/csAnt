dc7bc1c4f4634ef95c3add174eef303bdf3be1fa
CompulsiveCoder <compulsivecoder@gmail.com>
2014-05-02 10:31:37 +1000

    Updated version: 0.4.58

--- src/SoftwareMonkeys.csAnt/AssemblyInfo.cs
+++ src/SoftwareMonkeys.csAnt/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.4.57.499")]
+[assembly: AssemblyVersion("0.5.15.300")]
