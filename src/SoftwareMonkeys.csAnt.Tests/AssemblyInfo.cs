4a439ba7a1b57228674aef3274950c3dc3b7a759
CompulsiveCoder <compulsivecoder@gmail.com>
2014-05-04 20:48:04 +1000

    Set version to: 0.5.17.0

--- src/SoftwareMonkeys.csAnt.Tests/AssemblyInfo.cs
+++ src/SoftwareMonkeys.csAnt.Tests/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.4.59.899")]
+[assembly: AssemblyVersion("0.5.17.100")]
