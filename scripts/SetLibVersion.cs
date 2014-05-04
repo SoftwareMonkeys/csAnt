de93ca698e83f78500ca5664364a3e9ac2d606be
CompulsiveCoder <compulsivecoder@gmail.com>
2014-05-04 19:44:04 +1000

    Added SetLibVersion script

---
+++ scripts/SetLibVersion.cs
@@ -0,0 +1,24 @@
+//css_ref ../lib/csAnt/bin/Release/net-40/SoftwareMonkeys.csAnt.SetUp.dll;
+
+using System;
+using System.IO;
+using SoftwareMonkeys.csAnt;
+using SoftwareMonkeys.csAnt.Projects;
+
+class SetLibVersion : BaseProjectScript
+{
+	public static void Main(string[] args)
+	{
+		new SetLibVersion().Start(args);
+	}
+	
+	public override bool Run(string[] args)
+	{
+        var id = args[0];
+        var version = args[1];
+
+        new LibraryVersioner().SetVersion(id, version);
+
+		return !IsError;
+	}
+}
