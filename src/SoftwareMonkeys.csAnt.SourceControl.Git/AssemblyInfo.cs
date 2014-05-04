a889b095fbfa4e5c78103d3bff7f0768f339e857
CompulsiveCoder <compulsivecoder@gmail.com>
2014-05-04 20:32:56 +1000

    Set version to: 0.5.16

--- src/SoftwareMonkeys.csAnt.SourceControl.Git/AssemblyInfo.cs
+++ src/SoftwareMonkeys.csAnt.SourceControl.Git/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.4.59")]
+[assembly: AssemblyVersion("0.4.59.899")]
