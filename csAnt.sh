c47a606906190c9d38adb9e8aab244cdf475f13f
CompulsiveCoder <compulsivecoder@gmail.com>
2014-12-01 13:21:50 +1030

    Fixed csAnt.sh launcher script

--- csAnt.sh
+++ csAnt.sh
@@ -1,3 +1,3 @@
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR=$( cd "$( dirname '${BASH_SOURCE[0]}' )" && pwd )
 
-mono $DIR/lib/csAnt/bin/Release/net-40/csAnt.exe "$@" -b=$DIR
+mono "$DIR/lib/csAnt/bin/Release/net-40/csAnt.exe" "$@" -b=$DIR
