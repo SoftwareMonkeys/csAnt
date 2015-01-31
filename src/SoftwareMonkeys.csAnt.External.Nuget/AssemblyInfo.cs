05a20836900c44a5d898cd3ef5be0a51e34f0850
CompulsiveCoder <compulsivecoder@gmail.com>
2015-02-01 09:55:02 +1100

    Set version to: 0.5.43.800 (alpha)

--- src/SoftwareMonkeys.csAnt.External.Nuget/AssemblyInfo.cs
+++ src/SoftwareMonkeys.csAnt.External.Nuget/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.5.43.300")]
+[assembly: AssemblyVersion("0.5.43.800")]
